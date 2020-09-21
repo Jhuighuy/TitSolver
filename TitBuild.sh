8d8904a6530de708b32e1be69a337ac3ab6f0b9b
Oleg Butakov <butakovoleg@gmail.com>
2020-09-21 08:50:53 +0300

    Files reorganization

---
+++ TitBuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#clang++ sph.cpp -std=c++17 -Ofast -march=native -o Tit.out && echo 'compiled.'
+g++-10 src/sph.cpp -std=c++17 -Ofast -march=native -o Tit.out && echo 'compiled.'
+#g++-10 sph.cpp -std=c++17 -O0 -g -o Tit.out && echo 'compiled.'
--- src/TitBuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++-10 sph.cpp -std=c++17 -Ofast -march=native -o Tit.out && echo 'compiled.'
-#g++-10 sph.cpp -std=c++17 -O0 -g -o Tit.out && echo 'compiled.'
