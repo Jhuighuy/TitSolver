2bd155f7d53c8fa6aaedac6154c3eb02912f7e08
Oleg Butakov <butakovoleg@gmail.com>
2023-07-11 17:34:22 +0200

    Better dam breaking setup.

--- src/sph.cpp
+++ src/sph.cpp
@@ -49,9 +49,9 @@ int main() {
     });
   }
 
-  for (size_t n = 0; n < 3 * 3000; ++n) {
+  for (size_t n = 0; n < 3 * 2500; ++n) {
     std::cout << n << std::endl;
-    constexpr double dt = 0.00005 * 2500 / 3000;
+    constexpr double dt = 0.00005;
     timeint.step(dt, particles);
   }
 
@@ -71,37 +71,51 @@ int main() {
   using namespace tit;
   using namespace tit::sph;
 
+  const dim_t N = 100;
+  const real_t N_x = 4 * N;
+  const real_t N_y = 3 * N;
+  const real_t N_fixed = 3;
+  const real_t N_x_dam = 1 * N;
+  const real_t N_y_dam = 2 * N;
+
+  const real_t L = 1.0;
+  const real_t spacing = L / N;
+  const real_t timestep = 5.0e-5;
+  const real_t h_0 = 1.5 * spacing;
+  const real_t rho_0 = 1000.0;
+  const real_t cs_0 = 120.0;
+
   GradHSmoothEstimator estimator{
-      WeaklyCompressibleFluidEquationOfState{10.0, 1.0},
-      {},
-      {}};
+      WeaklyCompressibleFluidEquationOfState{cs_0, rho_0}, QuarticKernel{},
+      AlphaBetaArtificialViscosity{}};
   EulerIntegrator timeint{std::move(estimator)};
 
   using Vars = required_variables_t<decltype(estimator)>;
-  ParticleArray<double, 2, Vars> particles{};
-  for (size_t i = 0; i < 100; ++i) {
-    for (size_t j = 0; j < 100; ++j) {
-      const bool is_fixed = (i < 3 || i >= 97) || (j < 3 || j >= 97);
-      const bool is_water = (i < 40) && (j < 80);
+  ParticleArray<real_t, 2, Vars> particles{};
+  for (dim_t i = -N_fixed; i < N_x + N_fixed; ++i) {
+    for (dim_t j = -N_fixed; j < N_y; ++j) {
+      const bool is_fixed = (i < 0 || i >= N_x) || (j < 0);
+      const bool is_water = (i < N_x_dam) && (j < N_y_dam);
       if (!(is_fixed || is_water)) continue;
       particles.append([&]<class A>(A a) {
         using namespace particle_variables;
         fixed[a] = is_fixed;
-        m[a] = 1.0 / (3 * 40 * 80);
-        rho[a] = 1.0;
-        h[a] = 0.01;
-        r[a][0] = i / 100.0;
-        r[a][1] = j / 100.0;
+        rho[a] = rho_0;
+        m[a] = rho_0 / (N_x_dam * N_y_dam) * 2;
+        h[a] = h_0;
+        r[a][0] = i * spacing;
+        r[a][1] = j * spacing;
         if constexpr (has<A>(alpha)) alpha[a] = 1.0;
       });
     }
   }
 
-  for (size_t n = 0; n < 2000; ++n) {
+  particles.print("particles-dam.csv");
+
+  for (size_t n = 0; n < 100000; ++n) {
     std::cout << n << std::endl;
-    constexpr double dt = 0.00025;
-    timeint.step(dt, particles);
-    if (n % 10 == 0) particles.print("particles-dam.csv");
+    timeint.step(timestep, particles);
+    if (n % 100 == 0) particles.print("particles-dam.csv");
   }
 
   particles.print("particles-dam.csv");
