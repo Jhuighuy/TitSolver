cd70bce1e4a986ac0c865022d602633daf034015
Oleg Butakov <butakovoleg@gmail.com>
2023-10-01 13:59:33 +0200

    Draft for a few things.

--- src/tit/par/thread.hpp
+++ src/tit/par/thread.hpp
@@ -6,192 +6,215 @@
 
 #pragma once
 
-#include <algorithm>
+// #include <algorithm>
 #include <concepts>
-#include <cstdint>
-#include <iostream> // IWYU pragma: keep
+// #include <cstdint>
 #include <iterator>
 #include <ranges>
 
-#include "tit/core/config.hpp"
-#include "tit/core/types.hpp"
+#include <omp.h>
 
-// IWYU pragma: begin_keep
-#if TIT_ENABLE_TBB
-#include <oneapi/tbb/blocked_range.h>
-#include <oneapi/tbb/global_control.h>
-#include <oneapi/tbb/parallel_for.h>
-#include <oneapi/tbb/parallel_invoke.h>
-#include <oneapi/tbb/partitioner.h>
-#endif
-// IWYU pragma: end_keep
+#include "tit/core/config.hpp" // IWYU pragma: keep
+#include "tit/core/misc.hpp"   // IWYU pragma: keep
+#include "tit/core/types.hpp"
 
 namespace tit::par {
 
+struct impl_tag_t {};
+struct seg_tag_t : impl_tag_t {
+} seg_tag;
+struct par_tag_t : impl_tag_t {
+} par_tag;
+
+struct sched_tag_t {};
+struct static_tag_t : sched_tag_t {
+} static_tag;
+struct dynamic_tag_t : sched_tag_t {
+} dynamic_tag;
+
+template<class Tag>
+class Threading {
+public:
+
+  template<std::invocable Func>
+  static auto main_([[maybe_unused]] int argc, [[maybe_unused]] char** argv,
+                    Func&& func) -> int {
+    omp_set_num_threads(8);
+    return func();
+    // return func();
+  }
+
+  static constexpr auto num_threads_() noexcept -> size_t {
+    return 8;
+    // return 1;
+  }
+  static constexpr auto thread_index_() noexcept -> size_t {
+    return static_cast<size_t>(omp_get_thread_num());
+    // return 0;
+  }
+
+  template<std::invocable... Funcs>
+  static constexpr void invoke_(Funcs&&... funcs) noexcept {
+    (funcs(), ...);
+  }
+
+  template<class Range, class Func>
+  static void for_each_(dynamic_tag_t, Range&& range, Func&& func,
+                        [[maybe_unused]] size_t grain_size = 100) noexcept {
+    const auto end = std::ranges::end(range);
+#pragma omp parallel for schedule(dynamic, grain_size)
+    for (auto iter = std::ranges::begin(range); iter != end; ++iter)
+      func(*iter);
+  }
+
+  template<class Range, class Func>
+  constexpr static void for_each_(static_tag_t, Range&& range,
+                                  Func&& func) noexcept {
+    const auto end = std::ranges::end(range);
+#pragma omp parallel for schedule(static)
+    for (auto iter = std::ranges::begin(range); iter != end; ++iter)
+      func(*iter);
+  }
+
+  // template<std::ranges::input_range Range,
+  //          std::indirectly_unary_invocable<std::ranges::iterator_t<Range>>
+  //          Func>
+  // static constexpr void for_each_([[maybe_unused]] sched_tag_t shed_tag, //
+  //                                 Range&& range, Func&& func) noexcept {
+  //   std::ranges::for_each(range, func);
+  // }
+
+}; // class Threading
+
+// Generate a constexpr-aware overload for a threading function.
+#define TIT_THREAD_FUNC_IMPL_(func, ...)                                       \
+  if consteval {                                                               \
+    return Threading<seg_tag_t>::func(__VA_ARGS__);                            \
+  } else {                                                                     \
+    return Threading<par_tag_t>::func(__VA_ARGS__);                            \
+  }
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-#if TIT_ENABLE_TBB
-// Number of threads.
-size_t _num_threads = 8;
-// Index of the current thread.
-thread_local size_t _thread_index = SIZE_MAX;
-#endif
+/** Wrapper for the `main` that sets up multithreading. */
+template<std::invocable Func>
+auto main(int argc, char** argv, Func&& func) -> int {
+  TIT_THREAD_FUNC_IMPL_(main_, argc, argv, func);
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /** Number of threads. */
 constexpr auto num_threads() noexcept -> size_t {
-#if TIT_ENABLE_TBB
-  if consteval {
-#endif
-    return 1;
-#if TIT_ENABLE_TBB
-  } else {
-    return _num_threads;
-  }
-#endif
+  TIT_THREAD_FUNC_IMPL_(num_threads_);
 }
 
 /** Current thread index. */
 constexpr auto thread_index() noexcept -> size_t {
-#if TIT_ENABLE_TBB
-  if consteval {
-#endif
-    return 0;
-#if TIT_ENABLE_TBB
-  } else {
-    return _thread_index;
-  }
-#endif
-}
-
-/** Wrapper for the `main` that sets up parallelism. */
-template<std::invocable Func>
-auto main(int argc, char** argv, Func&& func) noexcept -> int {
-#if TIT_ENABLE_TBB
-  // TODO: correctly set maximum amount of cores.
-  tbb::global_control gc{tbb::global_control::max_allowed_parallelism,
-                         _num_threads};
-#endif
-  return func();
+  TIT_THREAD_FUNC_IMPL_(thread_index_);
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/** Invoke functions in parallel (inside the current process). */
+/** Invoke functions in parallel. */
 template<std::invocable... Funcs>
 constexpr void invoke(Funcs&&... funcs) noexcept {
-#if TIT_ENABLE_TBB
-  if consteval {
-#endif
-    (funcs(), ...);
-#if TIT_ENABLE_TBB
-  } else {
-    tbb::parallel_invoke(funcs...);
-  }
-#endif
+  TIT_THREAD_FUNC_IMPL_(invoke_, funcs...);
 }
 
-template<class Range>
-concept input_range = std::ranges::input_range<Range>;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-template<class Range, class Func>
-concept _can_par =
-    std::ranges::input_range<Range> && std::ranges::sized_range<Range> &&
-    std::indirectly_unary_invocable<Func, std::ranges::iterator_t<Range>>;
+/** Range that could be precessed in parallel. */
+/** @{ */
+template<class Range>
+concept base_input_range =
+    std::ranges::input_range<Range> &&
+    std::ranges::random_access_range<Range> && std::ranges::sized_range<Range>;
+template<class Range>
+concept input_range =
+    base_input_range<Range>
+// IWYU's clang has no `std::ranges::join_view`.
+#if !TIT_IWYU
+    || (specialization_of<Range, std::ranges::join_view> &&
+        base_input_range<decltype(std::declval<Range&&>().base())>)
+#endif
+    ;
+/** @} */
+
+template<class Func, class Range>
+concept loop_body =
+    base_input_range<Range> &&
+    std::indirectly_regular_unary_invocable<Func,
+                                            std::ranges::iterator_t<Range>>;
+
+template<class Func, class BaseRange>
+concept indirect_loop_body =
+    base_input_range<BaseRange> &&
+    std::indirectly_regular_unary_invocable<
+        Func, std::ranges::iterator_t<std::ranges::range_value_t<BaseRange>>>;
 
 /** Iterate through the range in parallel (dynamic partitioning). */
 /** @{ */
-template<class Range, class Func>
-  requires _can_par<Range, Func>
-constexpr void for_each(Range&& range, Func&& func,
-                        [[maybe_unused]] size_t grain_size = 1) noexcept {
-#if TIT_ENABLE_TBB
-  if consteval {
-#endif
-    std::ranges::for_each(range, func);
-#if TIT_ENABLE_TBB
-  } else {
-    const auto blocked_range =
-        tbb::blocked_range{range.begin(), range.end(), grain_size};
-    tbb::parallel_for(blocked_range, [&](auto subrange) {
-      std::ranges::for_each(subrange, func);
-    });
-  }
-#endif
+template<base_input_range Range, loop_body<Range> Func>
+constexpr void for_each(Range&& range, Func&& func) noexcept {
+  TIT_THREAD_FUNC_IMPL_(for_each_, dynamic_tag, range, func);
 }
 // IWYU's clang has no `std::ranges::join_view`.
 #if !TIT_IWYU
-template<class Range, class Func>
-constexpr void for_each(std::ranges::join_view<Range> range, Func&& func,
-                        size_t grain_size = 1) noexcept {
-  for_each(
-      std::move(range).base(),
-      [&](auto subrange) { std::ranges::for_each(subrange, func); },
-      grain_size);
+template<base_input_range BaseRange, indirect_loop_body<BaseRange> Func>
+constexpr void for_each( //
+    std::ranges::join_view<BaseRange> view, Func&& func) noexcept {
+  for_each(std::move(view).base(), [&](auto subrange) {
+    std::ranges::for_each(subrange, func); //
+  });
 }
 #endif
 /** @} */
 
 /** Iterate through the range in parallel (static partitioning). */
 /** @{ */
-template<class Range, class Func>
-  requires _can_par<Range, Func>
+template<base_input_range Range, loop_body<Range> Func>
 constexpr void static_for_each(Range&& range, Func&& func) noexcept {
-#if TIT_ENABLE_TBB
-  if consteval {
-#endif
-    std::ranges::for_each(range, func);
-#if TIT_ENABLE_TBB
-  } else {
-    const auto partition_size = range.size() / num_threads();
-    const auto partition_rem = range.size() % num_threads();
-    const auto partition_first = [&](size_t thread) {
-      return thread * partition_size + std::min(thread, partition_rem);
-    };
-    static auto partitioner = tbb::static_partitioner{};
-    tbb::parallel_for(
-        size_t{0}, num_threads(), size_t{1},
-        [&](size_t thread) {
-          _thread_index = thread;
-          std::for_each(range.begin() + partition_first(thread),
-                        range.begin() + partition_first(thread + 1), func);
-          _thread_index = SIZE_MAX;
-        },
-        partitioner);
-  }
-#endif
+  TIT_THREAD_FUNC_IMPL_(for_each_, static_tag, range, func);
 }
 // IWYU's clang has no `std::ranges::join_view`.
 #if !TIT_IWYU
-template<class Range, class Func>
-constexpr void static_for_each(std::ranges::join_view<Range> range,
-                               Func&& func) noexcept {
-  static_for_each(std::move(range).base(), [&](auto subrange) {
+template<base_input_range BaseRange, indirect_loop_body<BaseRange> Func>
+constexpr void static_for_each( //
+    std::ranges::join_view<BaseRange> view, Func&& func) noexcept {
+  static_for_each(std::move(view).base(), [&](auto subrange) {
     std::ranges::for_each(subrange, func);
   });
 }
 #endif
 /** @} */
 
-template<
-    std::ranges::input_range Range,
-    class /*std::indirectly_unary_invocable<std::ranges::iterator_t<Range>>*/
-    Func>
+template<class Range>
+concept block_input_range = std::ranges::input_range<Range>;
+
+template<block_input_range Range,
+         std::indirectly_regular_unary_invocable<
+             std::ranges::iterator_t<std::ranges::range_value_t<Range>>>
+             Func>
 constexpr void block_for_each(Range&& range, Func&& func) noexcept {
-#if TIT_ENABLE_TBB
-  invoke([&] { std::ranges::for_each(range[0], func); },
-         [&] { std::ranges::for_each(range[1], func); },
-         [&] { std::ranges::for_each(range[2], func); },
-         [&] { std::ranges::for_each(range[3], func); },
-         [&] { std::ranges::for_each(range[4], func); },
-         [&] { std::ranges::for_each(range[5], func); },
-         [&] { std::ranges::for_each(range[6], func); },
-         [&] { std::ranges::for_each(range[7], func); });
-  std::ranges::for_each(range[8], func);
-#else
-  for (auto x : range) for_each(x, func);
+  // IWYU's clang has no `std::views::chunk`.
+#if !TIT_IWYU
+  // Split the range in chuncks according to the number of threads and
+  // walk though the chunks sequentially.
+  const auto chuncked_range =
+      std::views::chunk(std::forward<Range>(range), num_threads());
+  for (auto&& chuck : chuncked_range) {
+    // Subranges inside each chunk are supposed to be indepenent thus
+    // are processed in parallel.
+    static_for_each(chuck, [&](auto subrange) {
+      std::ranges::for_each(subrange, func); //
+    });
+  }
 #endif
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 } // namespace tit::par
+
+// #include "tit/par/thread_omp.hpp"
