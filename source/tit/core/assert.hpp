dc59a23f99ba16c903b0b1089505446f5f816e53
Oleg Butakov <butakovoleg@gmail.com>
2023-11-11 23:49:26 +0100

    Better assertion macros.

--- source/tit/core/assert.hpp
+++ source/tit/core/assert.hpp
@@ -6,18 +6,58 @@
 
 #pragma once
 
-#include <cassert>
+#include <iostream>
+#include <source_location>
+#include <string_view>
 
 namespace tit {
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/** Assertion macro. */
-#ifndef NDEBUG
-#define TIT_ASSERT(expr, msg, ...) assert((expr) && (msg))
+/** "Assume" macro: assume that given expression always holds. Use this macro
+ ** to tell the compiler some not-obvious facts to better optimize the code. */
+#define TIT_ASSUME(expr) [[assume(expr)]]
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/** "Ensure" macro: ALWAYS (even in non-debug builds) check that given
+ ** expression holds. Do not use this macro for user input, but just to check
+ ** the internal logic. If the expression does not hold, the entire process
+ ** is aborted (in `constexpr` context an exception is thrown), so it this macro
+ ** is intended to be used inside of `noexcept` functions. */
+#define TIT_ENSURE(expr, message) (tit::_ensure((expr), #expr, (message)))
+
+/** "Assert" macro: ensure that given expression holds in debug builds,
+ ** and assume it always holds in non-debug build (which leads to better
+ ** optimizations). */
+#ifdef NDEBUG
+#define TIT_ASSERT(expr, message, ...) TIT_ASSUME((expr))
 #else
-#define TIT_ASSERT(expr, msg, ...) [[assume(expr)]]
+#define TIT_ASSERT(expr, message, ...) TIT_ENSURE((expr), (message))
 #endif
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// Abort the current process in non-`constexpr` context.
+[[noreturn]] inline void _ensure_failed( //
+    std::string_view expr_string, std::string_view message,
+    std::source_location location = std::source_location::current()) noexcept {
+  // TODO: print stack trace.
+  std::cerr << location.file_name() << ":" << location.line() << ":"
+            << location.column() << ": ";
+  std::cerr << "Internal consistency check failed: \"" << expr_string << "\". ";
+  std::cerr << message << "\n";
+  std::cerr.flush();
+  std::abort();
+}
+
+// Actual implementation of "Ensure" macro.
+constexpr void _ensure( //
+    bool expr_result, std::string_view expr_string, std::string_view message,
+    std::source_location location = std::source_location::current()) noexcept {
+  if (expr_result) return; // Gracefully return if check passes.
+  _ensure_failed(expr_string, message, location); // Abort process otherwise.
+}
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
