8e47bb9b3dc8f2cebc1a4528952014a3bacffe8d
Oleg Butakov <butakovoleg@gmail.com>
2023-11-16 09:35:17 +0100

    Add a small script that computes lines of code in the repository.

---
+++ build/cloc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Part of the Tit Solver project, under the MIT License
+# See /LICENSE.md for license information.
+# SPDX-License-Identifier: MIT
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# This script just prints and amount of lines of code in the git index.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Is help requrested?
+if [[ "$*" == *"-h"* || "$*" == *"--help"* ]]; then
+    echo "usage: $0 [path ...]"
+    echo ""
+    echo "  Count lines of code in the git indexed files."
+    exit 0
+fi
+
+# Count the lines of code.
+git ls-files $@ | xargs cloc
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
