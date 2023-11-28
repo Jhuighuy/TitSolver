afa804f1b4f9e16a46db51b64f389205ffcb27b8
Oleg Butakov <butakovoleg@gmail.com>
2023-11-28 01:00:44 +0100

    Refactor build script.

--- build/README.md
+++ build/README.md
@@ -1,3 +1,5 @@
 # `build`
 
-This directory contains build scripts.
+This directory contains build scripts. Keep Python scripts as simple as possible,
+since we are not making any requirements on the required version of Python. Most
+likely minimal required version is Python 3.9.
--- build/build.py
+++ build/build.py
@@ -12,173 +12,133 @@ import shutil
 import subprocess
 import sys
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-BLUE_COLOR_CODE = "\033[94m"
-GREEN_COLOR_CODE = "\033[92m"
-YELLOW_COLOR_CODE = "\033[93m"
-RED_COLOR_CODE = "\033[91m"
-RESET_COLOR_CODE = "\033[0m"
-
-
-def print_message(*args, **kwargs):
-    """Print message."""
-    print(BLUE_COLOR_CODE + "**", *args, RESET_COLOR_CODE, **kwargs)
-
-
-def print_success(*args, **kwargs):
-    """Print success message."""
-    print(GREEN_COLOR_CODE + "**", *args, RESET_COLOR_CODE, **kwargs)
-
-
-def print_warning(*args, **kwargs):
-    """Print warning."""
-    print(YELLOW_COLOR_CODE + "**", *args, RESET_COLOR_CODE, **kwargs)
-
-
-def print_error(*args, **kwargs):
-    """Print error."""
-    print(RED_COLOR_CODE + "**", *args, RESET_COLOR_CODE, **kwargs)
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
 HOME_DIR = os.path.expanduser("~")
 SOURCE_DIR = os.path.abspath(".")
 OUTPUT_DIR = os.path.join(SOURCE_DIR, "output", "cmake_output")
+TEST_DIR = os.path.join(OUTPUT_DIR, "tests")
+
+CMAKE_EXE = "cmake"
+CTEST_EXE = "ctest"
 
 
-def build():
-    """Build Tit."""
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-    # Parse command line arguments.
-    # (Do not start help for parameters with capital letter to match `-h`.)
-    parser = argparse.ArgumentParser(
-        description="Build Tit.",
-    )
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument(
         "-c",
         "--config",
         metavar="<config>",
+        help="build configuration",
         action="store",
         default="Release",
         choices=["Debug", "Release", "Coverage"],
-        help="build configuration",
     )
     parser.add_argument(
         "-f",
         "--force",
-        action="store_true",
         help="disable all static analysis during the build (for experimenting)",
+        action="store_true",
     )
     parser.add_argument(
         "-t",
         "--test",
-        action="store_true",
         help="run tests after successfully building the project",
+        action="store_true",
     )
     parser.add_argument(
         "-j",
         "--jobs",
-        metavar="N",
+        metavar="<num>",
+        help="number of threads to parallelize the build",
         action="store",
         nargs="?",
         type=int,
         const=0,
-        default=None,
-        help="number of threads to parallelize the build",
     )
     parser.add_argument(
         "--compiler",
-        metavar="EXE",
+        metavar="<path>",
+        help="override system's C++ compiler",
         action="store",
         default=None,
-        help="override system's C++ compiler",
     )
     parser.add_argument(
         "--vcpkg-root",
-        metavar="PATH",
+        metavar="<path>",
+        help="vcpkg package manager installation root path",
         action="store",
         default=None,
-        help="vcpkg package manager installation root path",
     )
     parser.add_argument(
         "--no-vcpkg",
-        action="store_true",
         help="do not use vcpkg for building the project",
-    )
-    parser.add_argument(
-        "--",
-        dest="extra_args",
-        metavar="<args>",
-        action="store",
-        nargs=argparse.REMAINDER,
-        default=None,
-        help="extra CMake arguments",
+        action="store_true",
     )
     parser.add_argument(
         "--dry",
-        action="store_true",
         help="perform a dry build: discard all previously built data.",
+        action="store_true",
     )
-    args = parser.parse_args()
+    parser.add_argument(
+        "--args",
+        metavar="<args>",
+        help="extra CMake configuration arguments",
+        dest="extra_args",
+        action="store",
+        nargs=argparse.REMAINDER,
+        default=None,
+    )
+    return parser.parse_args()
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-    # Dry build?
-    if args.dry:
-        print_warning("Performing a dry build.")
+def prepare_build_dir(dry=False):
+    """Prepare build directory."""
+    if dry:
+        print("Performing a dry build.")
         shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
-
-    # Create output directory.
     pathlib.Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-    # Prepare the CMake arguments for configuring.
-    cmake_args = ["cmake"]
+def find_vcpkg(hint=None):
+    """Find vcpkg installation"""
+    # Use our hint first, next search environment.
+    vcpkg_root = (
+        hint
+        or os.environ.get("VCPKG_ROOT")
+        or os.environ.get("VCPKG_INSTALLATION_ROOT")
+    )
+    # Next look for common directories.
+    if not vcpkg_root:
+        maybe_vcpkg_root = os.path.join(HOME_DIR, "vcpkg")
+        if os.path.exists(maybe_vcpkg_root) and os.path.isdir(maybe_vcpkg_root):
+            vcpkg_root = maybe_vcpkg_root
+    return vcpkg_root
 
+
+def configure(
+    config,
+    force=False,
+    compiler=None,
+    no_vcpkg=False,
+    vcpkg_root=None,
+    extra_args=None,
+):
+    """Configure the project."""
+    cmake_args = [CMAKE_EXE]
     # Setup the source and build directories.
     cmake_args += ["-S", SOURCE_DIR, "-B", OUTPUT_DIR]
-
     # Setup the build configuration.
-    config = args.config
-    print_success(f"Configuration: {config}.")
-    cmake_args.append("-D" + f"CMAKE_BUILD_TYPE={config}")
-
+    assert config
+    print(f"Configuration: {config}.")
+    cmake_args += ["-D" f"CMAKE_BUILD_TYPE={config}"]
     # Setup static analysis.
     # (Should be always passed, since `-D...` options are cached).
-    if args.force:
-        print_warning("'Force' build: static analysis is disabled.")
-    cmake_args.append("-D" + f"SKIP_ANALYSIS={'YES' if args.force else 'NO'}")
-
-    # Setup vcpkg.
-    if args.no_vcpkg:
-        print_message("Building without vcpkg.")
-        vcpkg_root = None
-    else:
-        vcpkg_root = (
-            args.vcpkg_root
-            or os.environ.get("VCPKG_ROOT")
-            or os.environ.get("VCPKG_INSTALLATION_ROOT")
-        )
-        if not vcpkg_root:
-            maybe_vcpkg_root = os.path.join(HOME_DIR, "vcpkg")
-            if os.path.exists(maybe_vcpkg_root) and os.path.isdir(maybe_vcpkg_root):
-                vcpkg_root = maybe_vcpkg_root
-        if vcpkg_root:
-            print_success(f"Using vcpkg root: {vcpkg_root}.")
-            vcpkg_toolchain_file = os.path.join(
-                vcpkg_root, "scripts", "buildsystems", "vcpkg.cmake"
-            )
-            cmake_args.append("-D" + f"CMAKE_TOOLCHAIN_FILE={vcpkg_toolchain_file}")
-        else:
-            print_error("Unable to find vcpkg.")
-            sys.exit(-1)
-
+    if force:
+        print("'Force' build: static analysis is disabled.")
+    cmake_args += ["-D", f"SKIP_ANALYSIS={'YES' if force else 'NO'}"]
     # Setup C++ compiler.
+    if compiler or os.environ.get("CXX"):
+        print(f"Overriding system's C++ compiler: {compiler}.")
     # To override the system compiler, there are two available approaches:
     #
     # 1. Specify it using CMake's `CMAKE_CXX_COMPILER` variable.
@@ -208,61 +168,84 @@ def build():
     #
     # In my view, using custom triplets solely to switch the compiler might be
     # excessive. Therefore, I'll stick to the environment variable method for now.
-    if compiler := (args.compiler or os.environ.get("CXX")):
-        print_warning(f"Overriding system's C++ compiler: {compiler}.")
-    if compiler := args.compiler:
+    if compiler:
         os.environ["CXX"] = compiler
-
-    # Append the extra arguments.
-    if args.extra_args:
-        cmake_args += args.extra_args
-
-    # Run CMake!
+    # Setup vcpkg.
+    if not no_vcpkg:
+        ## Find vcpkg.
+        vcpkg_root = find_vcpkg(hint=vcpkg_root)
+        if not vcpkg_root:
+            print("Unable to find vcpkg.")
+            sys.exit(-1)
+        ## Locate toolchain file.
+        toolchain_path = os.path.join(
+            vcpkg_root, "scripts", "buildsystems", "vcpkg.cmake"
+        )
+        if not os.path.exists(toolchain_path) or not os.path.isfile(toolchain_path):
+            print("Unable to find vcpkg toolchain file.")
+            sys.exit(-1)
+        cmake_args += ["-D", f"CMAKE_TOOLCHAIN_FILE={toolchain_path}"]
+    # Append extra arguments.
+    if extra_args:
+        cmake_args += extra_args
+    # Run CMake.
     if (exit_code := subprocess.call(cmake_args)) != 0:
         sys.exit(exit_code)
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-    # Prepare the CMake arguments for building.
+def build(config, jobs=0):
+    """Build project."""
     cmake_args = ["cmake"]
-
     # Setup build directory.
     cmake_args += ["--build", OUTPUT_DIR]
-
     # Setup the build configuration.
-    if config:
-        cmake_args += ["--config", config]
-
+    assert config
+    cmake_args += ["--config", config]
     # Setup the number of threads.
-    jobs = args.jobs
-    if jobs:
+    if jobs and jobs > 1:
         print(f"Building with {jobs} threads.")
-        cmake_args += ["--", "-j", str(jobs)]
-
-    # Run CMake!
+        cmake_args += ["-j", jobs]
+    # Run CMake.
     if (exit_code := subprocess.call(cmake_args)) != 0:
         sys.exit(exit_code)
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+def test(jobs=0):
+    """Test project."""
+    # Prepare CTest arguments.
+    ctest_args = ["ctest", "--output-on-failure"]
+    # Setup the number of threads.
+    if jobs and jobs > 1:
+        ctest_args += ["-j", jobs]
+    # Run CTest.
+    if (exit_code := subprocess.call(ctest_args, cwd=TEST_DIR)) != 0:
+        sys.exit(exit_code)
+
+
+def main():
+    """Build Tit."""
+    # Parse command line arguments.
+    args = parse_args()
+
+    # Prepare the build directory.
+    prepare_build_dir(dry=args.dry)
+
+    # Configure.
+    configure(
+        config=args.config,
+        force=args.force,
+        compiler=args.compiler,
+        no_vcpkg=args.no_vcpkg,
+        vcpkg_root=args.vcpkg_root,
+    )
+
+    # Build.
+    build(config=args.config, jobs=args.jobs)
+
+    # Test.
     if args.test:
-        # Prepare CTest arguments.
-        ctest_args = ["ctest", "--output-on-failure"]
-
-        # Setup the number of threads.
-        if jobs:
-            ctest_args += ["-j", str(jobs)]
-
-        # Prepare CTest working directory.
-        ctest_cwd = os.path.join(OUTPUT_DIR, "tests")
-
-        # Run CTest!
-        if (exit_code := subprocess.call(ctest_args, cwd=ctest_cwd)) != 0:
-            sys.exit(exit_code)
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+        test(jobs=args.jobs)
 
 
 if __name__ == "__main__":
-    build()
+    main()
