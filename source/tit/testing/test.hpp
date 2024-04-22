fb6495e25edd2ef88c4b26656a95e539ed6f7609
Oleg Butakov <butakovoleg@gmail.com>
2024-04-22 21:34:05 +0200

    Introduce `tit::testing` library.

---
+++ source/tit/testing/test.hpp
@@ -0,0 +1,12 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#pragma once
+
+// Some versions of doctest have broken platform detection. The easiest way to
+// fix it is to include this header before doctest.
+#include <cstdlib> // IWYU pragma: keep
+
+#include <doctest/doctest.h> // IWYU pragma: exports
