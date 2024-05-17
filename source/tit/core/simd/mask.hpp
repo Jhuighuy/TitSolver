821c0caada2dce20d61d5aca7a1320cd70992427
Oleg Butakov <butakovoleg@gmail.com>
2024-05-17 23:42:59 +0200

    Restore SIMD support with Highway.

---
+++ source/tit/core/simd/mask.hpp
@@ -0,0 +1,80 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+#include "tit/core/basic_types.hpp"
+#include "tit/core/simd/traits.hpp"
+
+namespace tit::simd {
+
+namespace impl {
+
+template<supported_type Num>
+struct mask_bits;
+
+template<std::integral Int>
+struct mask_bits<Int> : std::make_unsigned<Int> {};
+
+template<>
+struct mask_bits<float32_t> : std::type_identity<uint32_t> {};
+
+template<>
+struct mask_bits<float64_t> : std::type_identity<uint64_t> {};
+
+template<supported_type Num>
+using mask_bits_t = typename impl::mask_bits<Num>::type;
+
+} // namespace impl
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Scalar mask.
+template<supported_type Num>
+class Mask final {
+public:
+
+  /// Construct a mask equivalent to boolean @p b.
+  constexpr Mask(bool b = false) noexcept : bits_{b ? ~Bits{0} : Bits{0}} {}
+
+  /// Cast back to the boolean.
+  constexpr operator bool() const noexcept {
+    return bits_ != 0;
+  }
+
+  /// Mask negation operation.
+  friend constexpr auto operator!(Mask m) noexcept -> Mask {
+    return Mask{~m.bits_};
+  }
+
+  /// Mask conjunction operation.
+  friend constexpr auto operator&&(Mask m, Mask n) noexcept -> Mask {
+    return Mask{m.bits_ & n.bits_};
+  }
+
+  /// Mask disjunction operation.
+  friend constexpr auto operator||(Mask m, Mask n) noexcept -> Mask {
+    return Mask{m.bits_ | n.bits_};
+  }
+
+  /// Mask equality operation.
+  constexpr auto operator==(Mask const&) const noexcept -> bool = default;
+
+private:
+
+  using Bits = impl::mask_bits_t<Num>;
+
+  constexpr explicit Mask(Bits bits) noexcept : bits_{bits} {}
+
+  Bits bits_;
+
+}; // class Mask
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace tit::simd
---
+++ source/tit/core/simd/traits.hpp
@@ -0,0 +1,126 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#pragma once
+
+#include <bit>
+#include <concepts>
+#include <type_traits>
+
+#include "tit/core/basic_types.hpp"
+#include "tit/core/type_traits.hpp"
+
+namespace tit::simd {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Is SIMD supported for the specified numeric type?
+template<class Num>
+concept supported_type = std::integral<Num> || std::floating_point<Num>;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Minimal byte width of the SIMD register that is available on the current
+/// hardware.
+///
+/// That is 16 bytes for all supported instruction sets.
+inline constexpr size_t min_reg_byte_width_v{
+#ifdef __ARM_NEON
+    16
+#elifdef __SSE__
+    16
+#else
+    0
+#endif
+};
+
+/// Maximal byte width of the SIMD register that is available on the current
+/// hardware.
+///
+/// That is 16 bytes for NEON and SSE instruction set, 32 and 64 bytes for
+/// AVX and AVX-512 respectively.
+inline constexpr size_t max_reg_byte_width_v{
+#ifdef __ARM_NEON
+    16
+#elifdef __AVX512F__
+    64
+#elifdef __AVX__
+    32
+#elifdef __SSE__
+    16
+#else
+    0
+#endif
+};
+
+// Just in case.
+static_assert(max_reg_byte_width_v >= min_reg_byte_width_v &&
+              std::has_single_bit(max_reg_byte_width_v / min_reg_byte_width_v));
+
+/// Minimal SIMD register size for the specified type.
+template<supported_type Num>
+inline constexpr size_t min_reg_size_v = min_reg_byte_width_v / sizeof(Num);
+
+/// Maximal SIMD register size for the specified type.
+template<supported_type Num>
+inline constexpr size_t max_reg_size_v = max_reg_byte_width_v / sizeof(Num);
+
+/// Is SIMD supported for the specified numeric type and size?
+template<class Num, size_t Size>
+concept supported =
+    supported_type<Num> &&
+    in_range_v<Size, min_reg_size_v<Num>, max_reg_size_v<Num>> &&
+    (Size % min_reg_size_v<Num> == 0);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+namespace impl {
+
+// In Highway, type is identified as an integer, if it is one of the standard
+// fixed-width types. This leads to the problem that if, for example, `int64_t`
+// is defined as `long long` on some platforms, the specialization for `long`
+// would be missing. Even if `long` and `long long` have the same size, they are
+// not the same type:
+//
+// static_assert(sizeof(long) == sizeof(long long) &&
+//               !std::same_as<long, long long>);
+//
+// So, we need to map the incoming integral types to the corresponding
+// standard fixed-width types, leaving other types as they are.
+
+template<supported_type Num>
+struct fixed_width_type;
+
+template<supported_type Num>
+using fixed_width_type_t = typename fixed_width_type<Num>::type;
+
+template<std::unsigned_integral UInt>
+  requires (sizeof(UInt) == 1)
+struct fixed_width_type<UInt> : std::type_identity<uint8_t> {};
+
+template<std::unsigned_integral UInt>
+  requires (sizeof(UInt) == 2)
+struct fixed_width_type<UInt> : std::type_identity<uint16_t> {};
+
+template<std::unsigned_integral UInt>
+  requires (sizeof(UInt) == 4)
+struct fixed_width_type<UInt> : std::type_identity<uint32_t> {};
+
+template<std::unsigned_integral UInt>
+  requires (sizeof(UInt) == 8)
+struct fixed_width_type<UInt> : std::type_identity<uint64_t> {};
+
+template<std::signed_integral SInt>
+struct fixed_width_type<SInt> :
+    std::make_signed<fixed_width_type_t<std::make_unsigned_t<SInt>>> {};
+
+template<std::floating_point Float>
+struct fixed_width_type<Float> : std::type_identity<Float> {};
+
+} // namespace impl
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace tit::simd
