503126c9a5f8f9d1ce9de742bdb0f526bcabdc0e
Oleg Butakov <butakovoleg@gmail.com>
2024-06-19 13:15:59 +0200

    First attempts on GUI development.

--- front/src/components/App.test.tsx
+++ front/src/components/App.test.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { App } from "./app";
+import { App } from "./App";
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
