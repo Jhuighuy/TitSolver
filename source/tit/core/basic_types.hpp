ded0364b71c308832bae4dd29d5ce26cd3a24662
Oleg Butakov <butakovoleg@gmail.com>
2024-06-24 23:08:11 +0200

    Clean-up basic types.

--- source/tit/core/basic_types.hpp
+++ source/tit/core/basic_types.hpp
@@ -12,20 +12,32 @@ namespace tit {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+/// 8-bit signed integer type.
+using std::int8_t;
+
 /// 8-bit unsigned integer type.
-using uint8_t = std::uint8_t;
+using std::uint8_t;
+
+/// 16-bit signed integer type.
+using std::int16_t;
 
 /// 16-bit unsigned integer type.
-using uint16_t = std::uint16_t;
+using std::uint16_t;
+
+/// 32-bit signed integer type.
+using std::uint32_t;
 
 /// 32-bit unsigned integer type.
-using uint32_t = std::uint32_t;
+using std::uint32_t;
+
+/// 64-bit signed integer type.
+using std::int64_t;
 
 /// 64-bit unsigned integer type.
-using uint64_t = std::uint64_t;
+using std::uint64_t;
 
 /// Unsigned sized type.
-using size_t = std::size_t;
+using std::size_t;
 
 /// Signed sized type.
 using ssize_t = std::ptrdiff_t;
@@ -33,9 +45,11 @@ using ssize_t = std::ptrdiff_t;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// 32-bit floating point type.
+/// @todo Use definitions from <stdfloat> when it becomes available.
 using float32_t = float;
 
 /// 64-bit floating point type.
+/// @todo Use definitions from <stdfloat> when it becomes available.
 using float64_t = double;
 
 /// Default floating-point type type.
