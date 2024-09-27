e37548977ecba7590b23ca78541c48cb9e901988
Oleg Butakov <butakovoleg@gmail.com>
2024-09-27 16:03:28 +0200

    Use mi-malloc as our default allocator.

---
+++ source/tit/core/malloc.cpp
@@ -0,0 +1,6 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include <mimalloc-new-delete.h> // IWYU pragma: keep
