07adb67247a775c6f4e033b45e2e26345bbb3ebc
Oleg Butakov <butakovoleg@gmail.com>
2024-10-24 17:14:18 +0200

    Atomic CAS.

--- source/tit/core/par/atomic.hpp
+++ source/tit/core/par/atomic.hpp
@@ -19,7 +19,29 @@ namespace tit::par {
 template<class Val>
   requires std::integral<Val> || std::is_pointer_v<Val>
 auto fetch_and_add(Val& val, sub_result_t<Val> delta) noexcept -> Val {
-  return __atomic_fetch_add(&val, delta, __ATOMIC_RELAXED); // NOLINT(*-vararg)
+  // NOLINTNEXTLINE(*-pro-type-vararg)
+  return __atomic_fetch_add( //
+      &val,
+      delta,
+      /*memorder=*/__ATOMIC_RELAXED);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Atomically compare and exchange the value.
+template<class Val>
+  requires std::integral<Val> || std::is_pointer_v<Val>
+auto compare_exchange(Val& val,
+                      std::type_identity_t<Val> expected,
+                      std::type_identity_t<Val> desired) noexcept -> bool {
+  // NOLINTNEXTLINE(*-pro-type-vararg)
+  return __atomic_compare_exchange_n( //
+      &val,
+      &expected,
+      desired,
+      /*weak=*/false,
+      /*success_memorder=*/__ATOMIC_RELAXED,
+      /*failure_memorder=*/__ATOMIC_RELAXED);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
