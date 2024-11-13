c71381321009f78bec65cc41146a92d001e71fdc
Oleg Butakov <butakovoleg@gmail.com>
2024-11-13 20:50:25 +0100

    Additions to the parallel algorithms.

--- source/tit/testing/func_utils.hpp
+++ source/tit/testing/func_utils.hpp
@@ -5,8 +5,10 @@
 
 #pragma once
 
+#include <chrono>
 #include <concepts>
 #include <functional>
+#include <thread>
 #include <utility>
 
 #include "tit/core/basic_types.hpp"
@@ -42,6 +44,34 @@ private:
   size_t count_ = 0;
 
 }; // class CountedFunc
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// A wrapper for a function that sleeps for a given amount of time.
+template<class Func>
+class SleepFunc final {
+public:
+
+  /// Initialize a sleep function.
+  constexpr explicit SleepFunc(Func func,
+                               std::chrono::milliseconds duration =
+                                   std::chrono::milliseconds(10)) noexcept
+      : func_{std::move(func)}, duration_{duration} {}
+
+  /// Sleep for the specified amount of time and call the function.
+  template<class... Args>
+    requires std::invocable<Func&, Args&&...>
+  constexpr auto operator()(Args&&... args) const -> decltype(auto) {
+    std::this_thread::sleep_for(duration_);
+    return func_(std::forward<Args>(args)...);
+  }
+
+private:
+
+  Func func_;
+  std::chrono::milliseconds duration_;
+
+}; // class SleepFunc
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
