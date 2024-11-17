b95cab8f97a6a24fa822d157f3cda93d6a9637fb
Oleg Butakov <butakovoleg@gmail.com>
2024-11-17 12:53:55 +0100

    Implement a custom K-way graph partitioner.
    HEM may be broken.

--- source/tit/core/utils.hpp
+++ source/tit/core/utils.hpp
@@ -10,6 +10,7 @@
 #include <array>
 #include <concepts>
 #include <cstddef>
+#include <functional>
 #include <iterator>
 #include <ranges>
 #include <span>
@@ -75,7 +76,20 @@ constexpr auto is_any_of(T x, Us... us) noexcept -> bool {
   return (... || (x == us));
 }
 
+template<class T, class U>
+concept different_from =
+    !std::same_as<std::remove_cv_t<T>, std::remove_cv_t<U>>;
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Tuple-like type.
+template<class Tuple, class... Items>
+concept tuple_like = requires(Tuple& tuple, std::function<void(Items...)> f) {
+  {
+    std::tuple_size_v<std::remove_cvref_t<Tuple>>
+  } -> std::convertible_to<size_t>;
+  { std::apply(f, tuple) } -> std::same_as<void>;
+};
 
 /// Array type that is deduced from the given argument.
 /// If the argument is not an array, it is wrapped into an array of size 1.
@@ -212,6 +226,52 @@ constexpr void iota_perm(Range&& range, Perm&& perm) {
   std::ranges::copy(iota_perm(range), std::begin(perm));
 }
 /// @}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Container type.
+template<class Container>
+concept container = //
+    std::ranges::range<Container> &&
+    requires(std::remove_cvref_t<Container>& container,
+             size_t n,
+             std::ranges::range_value_t<Container> value) {
+      { container.assign(n, value) } -> std::same_as<void>;
+    };
+
+/// Range with value type.
+template<class Range, class Val>
+concept range_of = //
+    std::ranges::range<Range> &&
+    std::same_as<std::ranges::range_value_t<Range>, Val>;
+
+/// Find the equality ranges in the given range.
+template<
+    std::ranges::input_range Range,
+    std::invocable<std::ranges::subrange<std::ranges::iterator_t<Range>>> Func,
+    std::regular_invocable<std::ranges::range_reference_t<Range>> Proj =
+        std::identity,
+    std::indirect_binary_predicate<
+        std::projected<std::ranges::iterator_t<Range>, Proj>,
+        std::projected<std::ranges::iterator_t<Range>, Proj>> Pred =
+        std::equal_to<>>
+constexpr void equality_ranges(Range&& range,
+                               Func func,
+                               Pred pred = {},
+                               Proj proj = {}) {
+  TIT_ASSUME_UNIVERSAL(Range, range);
+  auto iter = std::begin(range);
+  const auto last = std::end(range);
+  while (iter != last) {
+    const auto next = std::ranges::find_if_not( //
+        iter,
+        last,
+        std::bind_front(std::ref(pred), std::invoke(proj, *iter)),
+        proj);
+    std::invoke(func, std::ranges::subrange{iter, next});
+    iter = next;
+  }
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
