3bac090bbf907ec33e4f1f69005451b2caa0ccca
Oleg Butakov <butakovoleg@gmail.com>
2024-11-22 12:22:19 +0100

    Skeleton for the backend. (#99)

---
+++ source/titback/backend.cpp
@@ -0,0 +1,57 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include <filesystem>
+#include <string>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#include <crow/app.h>
+#include <crow/http_request.h>
+#include <crow/http_response.h>
+#pragma GCC diagnostic pop
+
+#include "tit/core/main_func.hpp"
+#include "tit/core/sys_utils.hpp"
+
+namespace tit::back {
+namespace {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+auto run_backend(CmdArgs /*args*/) -> int {
+  const auto exe_dir = exe_path().parent_path();
+  const auto root_dir = exe_dir.parent_path();
+
+  crow::SimpleApp app;
+  CROW_ROUTE(app, "/")
+  ([&root_dir](const crow::request& /*request*/, crow::response& response) {
+    const auto index_html = root_dir / "frontend" / "index.html";
+    response.set_static_file_info_unsafe(index_html.native());
+    response.end();
+  });
+  CROW_ROUTE(app, "/<path>")
+  ([&root_dir](const crow::request& /*request*/,
+               crow::response& response,
+               const std::filesystem::path& file_name) {
+    auto file_path = root_dir / "frontend" / file_name;
+    if (std::filesystem::is_directory(file_path)) {
+      file_path /= "index.html";
+    }
+    response.set_static_file_info_unsafe(file_path.native());
+    response.end();
+  });
+
+  app.port(18080).run();
+
+  return 0;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace
+} // namespace tit::back
+
+TIT_IMPLEMENT_MAIN(back::run_backend)
