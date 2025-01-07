d190e3b98afc1bde8c7dbd639db05e71da80c23e
Oleg Butakov <butakovoleg@gmail.com>
2025-01-07 13:46:00 +0100

    Stop using nanobind and upgrade to Python 3.11.

---
+++ source/tit/py/_python.hpp
@@ -0,0 +1,22 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+ * Part of the Tit Solver project, under the MIT License.
+ * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// IWYU pragma: private
+// IWYU pragma: always_keep
+#pragma once
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Recommended Python configuration.
+#define PY_SSIZE_T_CLEAN
+
+// If we are not building the Python interpreter, we should use limited API.
+#ifndef TIT_PYTHON_INTERPRETER
+#define Py_LIMITED_API 0x030B0000 // Python 3.11.
+#endif
+
+#include <Python.h> // IWYU pragma: export
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
