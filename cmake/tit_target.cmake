57bd079d49aedcf4a4fd380dc161ba5fc3dea5bf
Oleg Butakov <butakovoleg@gmail.com>
2025-01-09 09:07:26 +0100

    Proper support for module targets.

--- cmake/tit_target.cmake
+++ cmake/tit_target.cmake
@@ -31,27 +31,27 @@ endfunction()
 #
 # Set the compile and link options for the given target.
 #
-function(configure_tit_target TARGET ACCESS)
+function(configure_tit_target TARGET VISIBILITY)
   # Setup include directories.
   target_include_directories(
-    ${TARGET} ${ACCESS}
+    ${TARGET} ${VISIBILITY}
     "${CMAKE_SOURCE_DIR}/source"
   )
 
   # Setup compile and link options.
-  target_compile_features(${TARGET} ${ACCESS} cxx_std_23)
+  target_compile_features(${TARGET} ${VISIBILITY} cxx_std_23)
   foreach(CONFIG ${ALL_CONFIGS})
     string(TOUPPER ${CONFIG} CONFIG_UPPER)
 
     # Apply compilation options.
     target_compile_options(
-      ${TARGET} ${ACCESS}
+      ${TARGET} ${VISIBILITY}
       $<$<CONFIG:${CONFIG}>:${CXX_COMPILE_OPTIONS_${CONFIG_UPPER}}>
     )
 
     # Apply link options.
     target_link_options(
-      ${TARGET} ${ACCESS}
+      ${TARGET} ${VISIBILITY}
       $<$<CONFIG:${CONFIG}>:${CXX_LINK_OPTIONS_${CONFIG_UPPER}}>
     )
   endforeach()
@@ -67,7 +67,7 @@ function(add_tit_library)
   cmake_parse_arguments(
     LIB
     "PUBLIC"
-    "NAME;TYPE"
+    "NAME;TYPE;PREFIX;SUFFIX;DESTINATION"
     "SOURCES;DEPENDS"
     ${ARGN}
   )
@@ -105,30 +105,43 @@ function(add_tit_library)
 
   # Setup visibility.
   if(LIB_TYPE STREQUAL "INTERFACE")
-    set(LIB_PUBLIC INTERFACE)
+    set(LIB_VISIBILITY INTERFACE)
   else()
-    set(LIB_PUBLIC PUBLIC)
+    set(LIB_VISIBILITY PUBLIC)
   endif()
 
-  # Create the library and the alias.
-  make_target_name(${LIB_NAME} LIB_TARGET)
-  set(LIB_TARGET_ALIAS "${TARGET_NAME_PREFIX}::${LIB_NAME}")
-  add_library(${LIB_TARGET} ${LIB_TYPE} ${LIB_SOURCES})
-  add_library(${LIB_TARGET_ALIAS} ALIAS ${LIB_TARGET})
+  # Create the library.
+  if(LIB_TYPE STREQUAL "MODULE")
+    set(LIB_TARGET ${LIB_NAME})
+    add_library(${LIB_TARGET} ${LIB_TYPE} ${LIB_SOURCES})
+  else()
+    make_target_name(${LIB_NAME} LIB_TARGET)
+    add_library(${LIB_TARGET} ${LIB_TYPE} ${LIB_SOURCES})
+    add_library("${TARGET_NAME_PREFIX}::${LIB_NAME}" ALIAS ${LIB_TARGET})
+  endif()
 
   # Configure the target.
-  configure_tit_target(${LIB_TARGET} ${LIB_PUBLIC})
+  configure_tit_target(${LIB_TARGET} ${LIB_VISIBILITY})
+  if(LIB_PREFIX OR LIB_SUFFIX)
+    set_target_properties(
+      ${LIB_TARGET} PROPERTIES
+      PREFIX "${LIB_PREFIX}"
+      SUFFIX "${LIB_SUFFIX}"
+    )
+  endif()
 
   # Link with the dependent libraries.
-  target_link_libraries(${LIB_TARGET} ${LIB_PUBLIC} ${LIB_DEPENDS})
+  target_link_libraries(${LIB_TARGET} ${LIB_VISIBILITY} ${LIB_DEPENDS})
 
   # Install the library.
   set(INSTALLABLE_LIB_TYPES SHARED MODULE)
   if(LIB_TYPE IN_LIST INSTALLABLE_LIB_TYPES)
-    if(LIB_PUBLIC)
-      set(LIB_DESTINATION "lib")
-    else()
-      set(LIB_DESTINATION "private/lib")
+    if(NOT LIB_DESTINATION)
+      if(LIB_PUBLIC)
+        set(LIB_DESTINATION "lib")
+      else()
+        set(LIB_DESTINATION "private/lib")
+      endif()
     endif()
     install(TARGETS ${LIB_TARGET} LIBRARY DESTINATION "${LIB_DESTINATION}")
   endif()
@@ -149,7 +162,7 @@ function(add_tit_executable)
   cmake_parse_arguments(
     EXE
     "PUBLIC"
-    "NAME;PREFIX"
+    "NAME;DESTINATION"
     "SOURCES;DEPENDS"
     ${ARGN}
   )
@@ -170,10 +183,12 @@ function(add_tit_executable)
   target_link_libraries(${EXE_TARGET} PRIVATE ${EXE_DEPENDS})
 
   # Install the executable.
-  if(EXE_PUBLIC)
-    set(EXE_DESTINATION "bin")
-  else()
-    set(EXE_DESTINATION "private/bin")
+  if(NOT EXE_DESTINATION)
+    if(EXE_PUBLIC)
+      set(EXE_DESTINATION "bin")
+    else()
+      set(EXE_DESTINATION "private/bin")
+    endif()
   endif()
   install(TARGETS ${EXE_TARGET} RUNTIME DESTINATION "${EXE_DESTINATION}")
 
