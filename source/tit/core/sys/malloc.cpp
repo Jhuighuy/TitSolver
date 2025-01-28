09ea80f749a29b58cdb7e2df458e7107a5c0fa3f
Oleg Butakov <butakovoleg@gmail.com>
2025-01-28 19:23:54 +0100

    Drop mimalloc, it causes interference with Python.

--- source/tit/core/sys/malloc.cpp
+++
@@ -1,6 +0,0 @@
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
- * Part of the Tit Solver project, under the MIT License.
- * See /LICENSE.md for license information. SPDX-License-Identifier: MIT
-\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-#include <mimalloc-new-delete.h> // IWYU pragma: keep
