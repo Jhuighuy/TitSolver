SPHQ/SPHQ-Solver

Python class bindings.

tests/tit/py/test_module.cpp
@@ -4,10 +4,15 @@
 \* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include <format>
+#include <iostream>
 #include <stdexcept>
 
+#include "tit/py/class.hpp"
 #include "tit/py/func.hpp"
 #include "tit/py/module.hpp"
+#include "tit/py/number.hpp"
+#include "tit/py/object.hpp"
+#include "tit/py/sequence.hpp"
 
 namespace tit {
 namespace {
@@ -26,6 +31,25 @@ void bind_test_module(py::Module& m) {
         py::Param<int, "a", 1>,
         py::Param<int, "b", 2>>();
   m.def<"throw", []() { throw std::logic_error{"Exception from C++!"}; }>();
+
+  class TestClass {};
+  const py::Class<TestClass> c{"TestClass", m};
+  c.def<"__init__",
+        [](TestClass& /*self*/, int a) {
+          std::cout << "__init__ " << a << std::endl; // NOLINT
+        },
+        py::Param<int, "a">>();
+  c.def<"hello_w", [](const TestClass& self) {
+    return py::make_tuple(py::Str{"Hello, class!"}, py::find(self));
+  }>();
+  c.prop<"a",
+         [](const TestClass& /*self*/) { return py::Int{14}; },
+         [](const TestClass& /*self*/, const py::Object& value) {
+           std::cout << "set a = " << py::extract<int>(value)
+                     << std::endl; // NOLINT
+         }>();
+
+  m.add(c);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Jhuighuy/TitSolver

tests/tit/py/test_module.cpp
@@ -4,10 +4,16 @@
 \* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include <format>
+#include <iostream>
 #include <stdexcept>
 
+#include "tit/py/cast.hpp"
+#include "tit/py/class.hpp"
 #include "tit/py/func.hpp"
 #include "tit/py/module.hpp"
+#include "tit/py/number.hpp"
+#include "tit/py/object.hpp"
+#include "tit/py/sequence.hpp"
 
 namespace tit {
 namespace {
@@ -26,6 +32,25 @@ void bind_test_module(py::Module& m) {
         py::Param<int, "a", 1>,
         py::Param<int, "b", 2>>();
   m.def<"throw", []() { throw std::logic_error{"Exception from C++!"}; }>();
+
+  class TestClass {};
+  const py::Class<TestClass> c{"TestClass", m};
+  c.def<"__init__",
+        [](TestClass& /*self*/, int a) {
+          std::cout << "__init__ " << a << std::endl; // NOLINT
+        },
+        py::Param<int, "a">>();
+  c.def<"hello_w", [](const TestClass& self) {
+    return py::make_tuple(py::Str{"Hello, class!"}, py::find(self));
+  }>();
+  c.prop<"a",
+         [](const TestClass& /*self*/) { return py::Int{14}; },
+         [](const TestClass& /*self*/, const py::Object& value) {
+           std::cout << "set a = " << py::cast<int>(value)
+                     << std::endl; // NOLINT
+         }>();
+
+  m.add(c);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~