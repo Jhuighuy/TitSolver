27e0f5aa698abbda17e4c2ad7fc5632ed424c506
Oleg Butakov <butakovoleg@gmail.com>
2025-03-05 11:34:59 +0300

    ParaView `.ttdb` reader. (#128)

---
+++ source/ttdb/TTDBReader.py
@@ -0,0 +1,169 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+# Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# pylint: disable=import-error,no-name-in-module,invalid-name,unused-argument
+# pylint: disable=wrong-import-position,missing-docstring
+from operator import attrgetter
+import os
+import sys
+from paraview.util.vtkAlgorithm import (  # type: ignore
+    smdomain,
+    smhint,
+    smproperty,
+    smproxy,
+    VTKPythonAlgorithmBase,
+)
+from vtk import (  # type: ignore
+    vtkCellArray,
+    vtkDataArraySelection,
+    vtkPoints,
+    vtkPolyData,
+    vtkVertex,
+)
+from vtkmodules.util import numpy_support
+
+# ParaView does not know how to import `ttdb.py`.
+sys.path.append(os.path.dirname(__file__))
+from ttdb import Rank, Storage, TimeStep  # noqa: E402
+
+__all__ = ("TTDBReader",)
+
+READER_LABEL = "BlueTit Database Reader"
+FILE_DESCRIPTION = "BlueTit Database"
+FILE_EXTENSIONS = "ttdb"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+@smproxy.reader(
+    name="TTDBReader",
+    label=READER_LABEL,
+    extensions=FILE_EXTENSIONS,
+    file_description=FILE_DESCRIPTION,
+)
+class TTDBReader(VTKPythonAlgorithmBase):
+    _file_path: str | None
+    _storage: Storage | None
+    _time_steps: list[TimeStep] | None
+    _array_selection: vtkDataArraySelection
+
+    def __init__(self) -> None:
+        super().__init__(nInputPorts=0, nOutputPorts=1)
+        self._file_path = None
+        self._storage = None
+        self._series = None
+        self._time_steps = None
+        self._array_selection = vtkDataArraySelection()
+        self._array_selection.AddObserver(
+            "ModifiedEvent", lambda *_: self.Modified()
+        )
+
+    @property
+    def storage(self) -> Storage:
+        if self._storage is None:
+            if not self._file_path or not os.path.exists(self._file_path):
+                raise RuntimeError(f"File not found: {self._file_path}.")
+            self._storage = Storage(self._file_path)
+            self._time_steps = None
+        return self._storage
+
+    @property
+    def time_steps(self) -> list[TimeStep]:
+        if self._time_steps is None:
+            self._time_steps = list(self.storage.last_series.time_steps())
+        return self._time_steps
+
+    @smproperty.stringvector(name="FileName")
+    @smdomain.filelist()
+    @smhint.filechooser(
+        extensions=FILE_EXTENSIONS, file_description=FILE_DESCRIPTION
+    )
+    def SetFileName(self, file_path: str) -> None:
+        if self._file_path == file_path:
+            return
+        self._file_path = file_path
+        self.Modified()
+
+    @smproperty.doublevector(
+        name="TimestepValues",
+        information_only="1",
+        si_class="vtkSITimeStepsProperty",
+    )
+    def GetTimestepValues(self) -> list[float]:
+        return list(map(attrgetter("time"), self.time_steps))
+
+    @smproperty.dataarrayselection(name="Arrays")
+    def GetDataArraySelection(self) -> vtkDataArraySelection:
+        return self._array_selection
+
+    def RequestInformation(self, request, inInfo, outInfo) -> bool:
+        executive = self.GetExecutive()
+
+        # Report the available time steps.
+        info = outInfo.GetInformationObject(0)
+        info.Remove(executive.TIME_RANGE())
+        info.Remove(executive.TIME_STEPS())
+        info.Append(executive.TIME_RANGE(), 0)
+        info.Append(executive.TIME_RANGE(), len(self.time_steps) - 1)
+        for ts in self.time_steps:
+            info.Append(executive.TIME_STEPS(), ts.time)
+
+        # Report the available arrays.
+        # ParaView does not support matrix arrays, so we do not report them.
+        probe_time_step = self.time_steps[0]
+        for array in probe_time_step.varyings.arrays():
+            if array.type.rank in (Rank.scalar, Rank.vector):
+                self._array_selection.AddArray(array.name)
+
+        return True
+
+    def RequestData(self, request, inInfo, outInfo) -> bool:
+        # Find the time step corresponding to the current time.
+        time: float = outInfo.GetInformationObject(0).Get(
+            self.GetExecutive().UPDATE_TIME_STEP()
+        )
+        time_step = next(
+            (ts for ts in self.time_steps if ts.time == time), None
+        )
+        if time_step is None:
+            return False
+
+        # Extract the data arrays.
+        arrays = {
+            array.name: array.data
+            for array in time_step.varyings.arrays()
+            if array.name == "r"
+            or self._array_selection.ArrayIsEnabled(array.name)
+        }
+        points = arrays["r"]
+
+        # Create VTK points array and a VTK "vertex" cell.
+        vtk_vertex = vtkVertex()
+        vtk_points = vtkPoints()
+        for i, point in enumerate(points):
+            xyz = (*point, 0.0, 0.0)[:3]  # must always be 3D.
+            vtk_vertex.GetPointIds().InsertNextId(i)
+            vtk_points.InsertNextPoint(*xyz)
+
+        vtk_vertices = vtkCellArray()
+        vtk_vertices.InsertNextCell(vtk_vertex)
+        vtk_poly_data = vtkPolyData()
+        vtk_poly_data.SetPoints(vtk_points)
+        vtk_poly_data.SetVerts(vtk_vertices)
+
+        # Create VTK arrays for the selected arrays.
+        for name, array in arrays.items():
+            vtk_array = numpy_support.numpy_to_vtk(
+                array,
+                numpy_support.get_vtk_array_type(array.dtype),
+            )
+            vtk_array.SetName(name)
+            vtk_poly_data.GetPointData().AddArray(vtk_array)
+
+        vtkPolyData.GetData(outInfo, 0).ShallowCopy(vtk_poly_data)
+        return True
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ source/ttdb/ttdb.hpp
@@ -0,0 +1,98 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+ * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+ * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#pragma once
+
+// NOLINTBEGIN(*-reserved-identifier,cert-*,modernize-*)
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma GCC visibility push(default)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef uint32_t ttdb_type_t;
+
+const char* ttdb_type__kind(ttdb_type_t type);
+uint32_t ttdb_type__rank(ttdb_type_t type);
+uint32_t ttdb_type__dim(ttdb_type_t type);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef struct ttdb_array ttdb_array_t;
+
+void ttdb_array__close(ttdb_array_t* array);
+const char* ttdb_array__name(ttdb_array_t* array);
+uint64_t ttdb_array__size(ttdb_array_t* array);
+ttdb_type_t ttdb_array__type(ttdb_array_t* array);
+void ttdb_array__read(ttdb_array_t* array, void* data);
+
+typedef struct ttdb_array_iter ttdb_array_iter_t;
+
+void ttdb_array_iter__close(ttdb_array_iter_t* iter);
+ttdb_array_t* ttdb_array_iter__next(ttdb_array_iter_t* iter);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef struct ttdb_dataset ttdb_dataset_t;
+
+void ttdb_dataset__close(ttdb_dataset_t* dataset);
+uint64_t ttdb_dataset__num_arrays(ttdb_dataset_t* dataset);
+ttdb_array_t* ttdb_dataset__find_array(ttdb_dataset_t* dataset,
+                                       const char* name);
+ttdb_array_iter_t* ttdb_dataset__arrays(ttdb_dataset_t* dataset);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef struct ttdb_time_step ttdb_time_step_t;
+
+void ttdb_time_step__close(ttdb_time_step_t* time_step);
+double ttdb_time_step__time(ttdb_time_step_t* time_step);
+ttdb_dataset_t* ttdb_time_step__uniforms(ttdb_time_step_t* time_step);
+ttdb_dataset_t* ttdb_time_step__varyings(ttdb_time_step_t* time_step);
+
+typedef struct ttdb_time_step_iter ttdb_time_step_iter_t;
+
+void ttdb_time_step_iter__close(ttdb_time_step_iter_t* iter);
+ttdb_time_step_t* ttdb_time_step_iter__next(ttdb_time_step_iter_t* iter);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef struct ttdb_series ttdb_series_t;
+
+void ttdb_series__close(ttdb_series_t* series);
+uint64_t ttdb_series__num_time_steps(ttdb_series_t* series);
+ttdb_time_step_t* ttdb_series__last_time_step(ttdb_series_t* series);
+ttdb_time_step_iter_t* ttdb_series__time_steps(ttdb_series_t* series);
+
+typedef struct ttdb_series_iter ttdb_series_iter_t;
+
+void ttdb_series_iter__close(ttdb_series_iter_t* iter);
+ttdb_series_t* ttdb_series_iter__next(ttdb_series_iter_t* iter);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+typedef struct ttdb ttdb_t;
+
+void ttdb__close(ttdb_t* db);
+ttdb_t* ttdb__open(const char* path);
+uint64_t ttdb__num_series(ttdb_t* db);
+ttdb_series_t* ttdb__last_series(ttdb_t* db);
+ttdb_series_iter_t* ttdb__series(ttdb_t* db);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#pragma GCC visibility pop
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+// NOLINTEND(*-reserved-identifier,cert-*,modernize-*)
