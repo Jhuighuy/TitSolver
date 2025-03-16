20d864546384b39fc09f5c27b920699a3d5edce0
Oleg Butakov <butakovoleg@gmail.com>
2025-03-17 00:23:47 +0100

    Build: Restore proper copyright banners.

--- source/tit/core/utils.hpp
+++ source/tit/core/utils.hpp
@@ -1,9 +1,8 @@
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
  * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
  * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
 \* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-/// @todo Restore proper banners once Clang is fixed.
 #pragma once
 
 #include <algorithm>
