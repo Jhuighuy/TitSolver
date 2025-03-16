9d58842d8299c02ac6a720d83ff3dd9d2ab990a9
Oleg Butakov <butakovoleg@gmail.com>
2025-03-17 00:23:47 +0100

    Build: Restore proper copyright banners.

--- source/tit/core/par/memory_pool.test.cpp
+++ source/tit/core/par/memory_pool.test.cpp
@@ -1,4 +1,4 @@
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
  * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
  * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
 \* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
