67eba22d14020e28a18840a7508c82cbf73dd8e9
Oleg Butakov <butakovoleg@gmail.com>
2025-03-20 01:30:20 +0100

    Core: Add Boost.Geometry traits for the vector class.

--- source/tit/core/_vec/traits.hpp
+++ source/tit/core/_vec/traits.hpp
@@ -8,6 +8,15 @@
 
 #include <type_traits>
 
+// Boost.Geometry is heavy-weight, so we only include the parts we need.
+#include <boost/geometry/core/access.hpp>
+#include <boost/geometry/core/coordinate_dimension.hpp>
+#include <boost/geometry/core/coordinate_system.hpp>
+#include <boost/geometry/core/coordinate_type.hpp>
+#include <boost/geometry/core/cs.hpp>
+#include <boost/geometry/core/tag.hpp>
+#include <boost/geometry/core/tags.hpp>
+
 #include "tit/core/_vec/vec.hpp"
 #include "tit/core/basic_types.hpp"
 
@@ -39,3 +48,33 @@ inline constexpr size_t vec_dim_v = Vec{}.dim();
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 } // namespace tit
+
+namespace bgt = boost::geometry::traits;
+
+template<class Num, tit::size_t Dim>
+struct bgt::tag<tit::Vec<Num, Dim>> final : std::type_identity<point_tag> {};
+
+template<class Num, tit::size_t Dim>
+struct bgt::dimension<tit::Vec<Num, Dim>> final :
+    std::integral_constant<tit::size_t, Dim> {};
+
+template<class Num, tit::size_t Dim>
+struct bgt::coordinate_type<tit::Vec<Num, Dim>> final :
+    std::type_identity<Num> {};
+
+template<class Num, tit::size_t Dim>
+struct bgt::coordinate_system<tit::Vec<Num, Dim>> final :
+    std::type_identity<cs::cartesian> {};
+
+template<class Num, tit::size_t Dim, tit::size_t Index>
+struct bgt::access<tit::Vec<Num, Dim>, Index> {
+  static_assert(Index < Dim, "Index is out of range!");
+  constexpr static auto get(const tit::Vec<Num, Dim>& a) noexcept -> Num {
+    return a[Index];
+  }
+  constexpr static void set(tit::Vec<Num, Dim>& a, Num v) noexcept {
+    a[Index] = std::move(v);
+  }
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
