e74e7a7fcd52ded153eff16166760a944d80df02
Oleg Butakov <butakovoleg@gmail.com>
2025-04-28 10:23:33 +0200

    Core: Clean-up environment utils.

--- source/titback/backend.cpp
+++ source/titback/backend.cpp
@@ -18,6 +18,7 @@
 #include "tit/core/basic_types.hpp"
 #include "tit/core/checks.hpp"
 #include "tit/core/cmd.hpp"
+#include "tit/core/env.hpp"
 #include "tit/core/sys/utils.hpp"
 
 #include "tit/core/type.hpp"
