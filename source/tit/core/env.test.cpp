4471dce361dd91e8c037bc9e07cf378f83268e88
Oleg Butakov <butakovoleg@gmail.com>
2025-04-28 10:23:33 +0200

    Core: Clean-up environment variables handling.

---
+++ source/tit/core/env.test.cpp
@@ -0,0 +1,76 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+ * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "tit/core/env.hpp"
+#include "tit/core/exception.hpp"
+
+#include "tit/testing/test.hpp"
+
+namespace tit {
+namespace {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+TEST_CASE("get_env") {
+  CHECK(get_env("PATH").has_value());
+  CHECK(get_env("HOME").has_value());
+  CHECK_FALSE(get_env("DOES_NOT_EXIST").has_value());
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+TEST_CASE("set_env") {
+  SUBCASE("string") {
+    SUBCASE("valid") {
+      set_env("TEST_VAR", "TEST");
+      CHECK(get_env("TEST_VAR") == "TEST");
+    }
+    SUBCASE("invalid") {
+      CHECK_THROWS_MSG(
+          set_env("TEST=VAR", "TEST"),
+          Exception,
+          "Unable to set environment variable 'TEST=VAR' to 'TEST'.");
+    }
+  }
+  SUBCASE("int") {
+    SUBCASE("positive") {
+      set_env("TEST_VAR", 123);
+      CHECK(get_env<int>("TEST_VAR") == 123);
+    }
+    SUBCASE("negative") {
+      set_env("TEST_VAR", -123);
+      CHECK(get_env<int>("TEST_VAR") == -123);
+    }
+    SUBCASE("invalid") {
+      set_env("TEST_VAR", "TEST");
+      CHECK_THROWS_MSG(get_env<int>("TEST_VAR"),
+                       Exception,
+                       "Invalid value of environment variable 'TEST_VAR=TEST'. "
+                       "Must be 'int'.");
+    }
+  }
+  SUBCASE("bool") {
+    SUBCASE("false") {
+      set_env("TEST_VAR", "false");
+      CHECK(get_env<bool>("TEST_VAR") == false);
+    }
+    SUBCASE("true") {
+      set_env("TEST_VAR", "true");
+      CHECK(get_env<bool>("TEST_VAR") == true);
+    }
+    SUBCASE("invalid") {
+      set_env("TEST_VAR", "TEST");
+      CHECK_THROWS_MSG(get_env<bool>("TEST_VAR"),
+                       Exception,
+                       "Invalid value of environment variable 'TEST_VAR=TEST'. "
+                       "Must be 'bool'.");
+    }
+  }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace
+} // namespace tit
