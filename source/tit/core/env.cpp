dd7bd59d08c9cdb681539fe72d7a7e1f1be85bbf
Oleg Butakov <butakovoleg@gmail.com>
2025-04-28 10:23:33 +0200

    Core: Clean-up environment variables handling.

---
+++ source/tit/core/env.cpp
@@ -0,0 +1,38 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+ * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include <cstdlib>
+#include <optional>
+
+#include "tit/core/env.hpp"
+#include "tit/core/exception.hpp"
+#include "tit/core/par/control.hpp"
+#include "tit/core/str.hpp"
+
+namespace tit {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void set_env(CStrView name, CStrView val) {
+  const par::GlobalLock lock{}; // NOLINTNEXTLINE(*-mt-unsafe,*-include-cleaner)
+  const auto status = setenv(name.c_str(), val.c_str(), /*overwrite=*/1);
+  TIT_ENSURE(status == 0,
+             "Unable to set environment variable '{}' to '{}'.",
+             name,
+             val);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+auto get_env(CStrView name) noexcept -> std::optional<CStrView> {
+  const par::GlobalLock lock{};
+  const auto* const value = std::getenv(name.c_str()); // NOLINT(*-mt-unsafe)
+  if (value == nullptr) return std::nullopt;
+  return CStrView{value};
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace tit
