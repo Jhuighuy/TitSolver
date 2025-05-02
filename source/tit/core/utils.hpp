81ee07c949f32615e9536e7c9b79dfaf87f08faf
Oleg Butakov <butakovoleg@gmail.com>
2025-05-02 17:50:34 +0200

    Core: Clean-up math utils.

--- source/tit/core/utils.hpp
+++ source/tit/core/utils.hpp
@@ -52,6 +52,12 @@ struct AlwaysTrue final {
   }
 };
 
+/// Default-initialize a type.
+template<std::default_initializable T>
+constexpr auto zero(const T& /*a*/) -> T {
+  return T{};
+}
+
 /// Check if the given value is in the range [ @p a, @p b ].
 template<class T>
 constexpr auto in_range(const T& x,
