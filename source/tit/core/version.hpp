413012d7604437bd1ddd555467643b258b8b0655
Oleg Butakov <butakovoleg@gmail.com>
2025-05-05 10:36:07 +0200

    Core: Build information.

---
+++ source/tit/core/version.hpp
@@ -0,0 +1,25 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+ * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#pragma once
+
+#include "tit/core/str.hpp"
+
+namespace tit::version {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/// Build version.
+extern const CStrView version;
+
+/// Hash of the last commit.
+extern const CStrView commit_hash;
+
+/// Date of the last commit.
+extern const CStrView commit_date;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+} // namespace tit::version
