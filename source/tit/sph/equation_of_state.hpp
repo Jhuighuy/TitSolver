74b68dec1a6a4230ad619b41ce622a085d5a4c5e
Oleg Butakov <butakovoleg@gmail.com>
2025-05-08 10:41:20 +0200

    SPH: Do not hardcode floating point type.

--- source/tit/sph/equation_of_state.hpp
+++ source/tit/sph/equation_of_state.hpp
@@ -9,7 +9,6 @@
 #include <concepts>
 #include <utility>
 
-#include "tit/core/basic_types.hpp"
 #include "tit/core/checks.hpp"
 #include "tit/core/math.hpp"
 #include "tit/core/type.hpp"
@@ -22,6 +21,7 @@ namespace tit::sph {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// Ideal gas equation of state.
+template<class Num>
 class IdealGasEquationOfState final {
 public:
 
@@ -34,32 +34,33 @@ public:
   /// Construct an equation of state.
   ///
   /// @param gamma Adiabatic index.
-  constexpr explicit IdealGasEquationOfState(real_t gamma = 1.4) noexcept
+  constexpr explicit IdealGasEquationOfState(Num gamma = 1.4) noexcept
       : gamma_{gamma} {
     TIT_ASSERT(gamma_ > 1.0, "Adiabatic index must be greater than 1!");
   }
 
   /// Pressure value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto pressure(PV a) const noexcept {
     return (gamma_ - 1.0) * rho[a] * u[a];
   }
 
   /// Sound speed value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto sound_speed(PV a) const noexcept {
     return sqrt(gamma_ * (gamma_ - 1.0) * u[a]); // == sqrt(gamma * p / rho).
   }
 
 private:
 
-  real_t gamma_;
+  Num gamma_;
 
 }; // class IdealGasEquationOfState
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// Adiabatic ideal gas equation of state.
+template<class Num>
 class AdiabaticIdealGasEquationOfState final {
 public:
 
@@ -73,30 +74,29 @@ public:
   ///
   /// @param kappa Thermal conductivity coefficient.
   /// @param gamma Adiabatic index.
-  constexpr explicit AdiabaticIdealGasEquationOfState(
-      real_t kappa = 1.0,
-      real_t gamma = 1.4) noexcept
+  constexpr explicit AdiabaticIdealGasEquationOfState(Num kappa = 1.0,
+                                                      Num gamma = 1.4) noexcept
       : kappa_{kappa}, gamma_{gamma} {
     TIT_ASSERT(kappa_ > 0.0, "Conductivity coefficient must be positive!");
     TIT_ASSERT(gamma_ > 1.0, "Adiabatic index must be greater than 1!");
   }
 
   /// Pressure value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto pressure(PV a) const noexcept {
     return kappa_ * pow(rho[a], gamma_);
   }
 
   /// Sound speed value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto sound_speed(PV a) const noexcept {
     return sqrt(kappa_ * pow(rho[a], gamma_)); // == sqrt(gamma * p / rho).
   }
 
 private:
 
-  real_t kappa_;
-  real_t gamma_;
+  Num kappa_;
+  Num gamma_;
 
 }; // class GasEquationOfState
 
@@ -108,7 +108,8 @@ public:
 
   /// Corrected density value.
   template<particle_view<rho> PV>
-  constexpr auto corrected_density(PV a, real_t /*rho_0*/) const noexcept {
+  constexpr auto corrected_density(PV a, particle_num_t<PV> /*rho_0*/)
+      const noexcept {
     return rho[a];
   }
 
@@ -120,7 +121,8 @@ public:
 
   /// Corrected density value.
   template<particle_view<rho> PV>
-  constexpr auto corrected_density(PV a, real_t rho_0) const noexcept {
+  constexpr auto corrected_density(PV a,
+                                   particle_num_t<PV> rho_0) const noexcept {
     const auto rho_a = rho[a];
     return a.has_type(ParticleType::fixed) ? std::max(rho_a, rho_0) : rho_a;
   }
@@ -133,7 +135,7 @@ concept pressure_correction = std::same_as<PC, NoCorrection> || //
                               std::same_as<PC, HughesGrahamCorrection>;
 
 /// Tait equation equation of state (for weakly-compressible fluids).
-template<pressure_correction Correction = HughesGrahamCorrection>
+template<class Num, pressure_correction Correction = HughesGrahamCorrection>
 class TaitEquationOfState final {
 public:
 
@@ -151,10 +153,10 @@ public:
   /// @param p_0        Background pressure.
   /// @param gamma      Polytropic index.
   /// @param correction Pressure correction method.
-  constexpr explicit TaitEquationOfState(real_t cs_0,
-                                         real_t rho_0,
-                                         real_t p_0 = 0.0,
-                                         real_t gamma = 7.0,
+  constexpr explicit TaitEquationOfState(Num cs_0,
+                                         Num rho_0,
+                                         Num p_0 = 0.0,
+                                         Num gamma = 7.0,
                                          Correction correction = {}) noexcept
       : cs_0_{cs_0}, rho_0_{rho_0}, p_0_{p_0}, gamma_{gamma},
         correction_{std::move(correction)} {
@@ -164,7 +166,7 @@ public:
   }
 
   /// Pressure value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto pressure(PV a) const noexcept {
     const auto B = rho_0_ * pow2(cs_0_) / gamma_;
     const auto rho_a = correction_.corrected_density(a, rho_0_);
@@ -172,7 +174,7 @@ public:
   }
 
   /// Sound speed value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto sound_speed(PV a) const noexcept {
     const auto rho_a = correction_.corrected_density(a, rho_0_);
     return cs_0_ * pow(rho_a / rho_0_, gamma_);
@@ -180,16 +182,16 @@ public:
 
 private:
 
-  real_t cs_0_;
-  real_t rho_0_;
-  real_t p_0_;
-  real_t gamma_;
+  Num cs_0_;
+  Num rho_0_;
+  Num p_0_;
+  Num gamma_;
   [[no_unique_address]] Correction correction_;
 
 }; // class TaitEquationOfState
 
 /// Linear Tait equation equation of state (for weakly-compressible fluids).
-template<pressure_correction Correction = HughesGrahamCorrection>
+template<class Num, pressure_correction Correction = HughesGrahamCorrection>
 class LinearTaitEquationOfState final {
 public:
 
@@ -206,9 +208,9 @@ public:
   /// @param rho_0      Reference density.
   /// @param p_0        Background pressure.
   /// @param correction Pressure correction method.
-  constexpr LinearTaitEquationOfState(real_t cs_0,
-                                      real_t rho_0,
-                                      real_t p_0 = 0.0,
+  constexpr LinearTaitEquationOfState(Num cs_0,
+                                      Num rho_0,
+                                      Num p_0 = 0.0,
                                       Correction correction = {}) noexcept
       : cs_0_{cs_0}, rho_0_{rho_0}, p_0_{p_0},
         correction_{std::move(correction)} {
@@ -217,23 +219,23 @@ public:
   }
 
   /// Pressure value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto pressure(PV a) const noexcept {
     const auto rho_a = correction_.corrected_density(a, rho_0_);
     return p_0_ + pow2(cs_0_) * (rho_a - rho_0_);
   }
 
   /// Sound speed value.
-  template<particle_view<required_fields> PV>
+  template<particle_view_n<Num, required_fields> PV>
   constexpr auto sound_speed(PV /*a*/) const noexcept {
     return cs_0_;
   }
 
 private:
 
-  real_t cs_0_;
-  real_t rho_0_;
-  real_t p_0_;
+  Num cs_0_;
+  Num rho_0_;
+  Num p_0_;
   [[no_unique_address]] Correction correction_;
 
 }; // class LinearTaitEquationOfState
@@ -243,8 +245,8 @@ private:
 /// Equation of state type.
 template<class EOS>
 concept equation_of_state =
-    std::same_as<EOS, IdealGasEquationOfState> ||
-    std::same_as<EOS, AdiabaticIdealGasEquationOfState> ||
+    specialization_of<EOS, IdealGasEquationOfState> ||
+    specialization_of<EOS, AdiabaticIdealGasEquationOfState> ||
     specialization_of<EOS, TaitEquationOfState> ||
     specialization_of<EOS, LinearTaitEquationOfState>;
 
