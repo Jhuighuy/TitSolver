2d4465bff6a8197895a5c18e70a960c63c52a4ca
Oleg Butakov <butakovoleg@gmail.com>
2025-05-19 15:36:49 +0200

    SPH: Drop required fields parameter for particle concepts.

--- source/tit/sph/viscosity.hpp
+++ source/tit/sph/viscosity.hpp
@@ -28,7 +28,7 @@ public:
   static constexpr TypeSet modified_fields{/*empty*/};
 
   /// Compute viscosity term.
-  template<particle_view<required_fields> PV>
+  template<particle_view PV>
   constexpr auto operator()(PV a, PV b) const noexcept {
     TIT_ASSERT(a != b, "Particles must be different!");
     return 0;
@@ -49,7 +49,7 @@ public:
   static constexpr TypeSet modified_fields{/*empty*/};
 
   /// Compute viscosity term.
-  template<particle_view<required_fields> PV>
+  template<particle_view PV>
   constexpr auto operator()(PV a, PV b) const noexcept {
     TIT_ASSERT(a != b, "Particles must be different!");
     const auto d = r[a].dim();
