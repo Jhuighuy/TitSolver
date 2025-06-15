732e096f99dfedc02b75f8f73a3d2fc7ad3f5938
Oleg Butakov <butakovoleg@gmail.com>
2025-06-15 23:35:05 +0200

    Core: Fix minimal SIMD register size on NEON.

--- source/tit/core/_simd/deduce.test.cpp
+++ source/tit/core/_simd/deduce.test.cpp
@@ -23,40 +23,77 @@ using DeducedRegArray = std::array<simd::deduce_reg_t<float, Dim>,
 
 #if defined __AVX512F__
 
-// Can be fitted into a 128 bit register.
+// Can be fitted into a single 128 bit register.
+static_assert(std::is_same_v<DeducedRegArray<1>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<2>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<3>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<4>, RegArray<4, 1>>);
 
-// Can be fitted into a 256 bit register.
+// Can be fitted into a single 256 bit register.
+static_assert(std::is_same_v<DeducedRegArray<5>, RegArray<8, 1>>);
+static_assert(std::is_same_v<DeducedRegArray<6>, RegArray<8, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<7>, RegArray<8, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<8>, RegArray<8, 1>>);
 
-// Should be a 512 bit register.
+// Can be fitted into a single 512 bit register.
+static_assert(std::is_same_v<DeducedRegArray<9>, RegArray<16, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<10>, RegArray<16, 1>>);
+static_assert(std::is_same_v<DeducedRegArray<16>, RegArray<16, 1>>);
+
+// Should occupy a few 512 bit registers.
+static_assert(std::is_same_v<DeducedRegArray<17>, RegArray<16, 2>>);
 static_assert(std::is_same_v<DeducedRegArray<20>, RegArray<16, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<32>, RegArray<16, 2>>);
 static_assert(std::is_same_v<DeducedRegArray<36>, RegArray<16, 3>>);
 
 #elif defined __AVX__
 
-// Can be fitted into a 128 bit register.
+// Can be fitted into a single 128 bit register.
+static_assert(std::is_same_v<DeducedRegArray<1>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<2>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<3>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<4>, RegArray<4, 1>>);
 
-// Should be a 256 bit register.
+// Can be fitted into a single 256 bit register.
+static_assert(std::is_same_v<DeducedRegArray<5>, RegArray<8, 1>>);
+static_assert(std::is_same_v<DeducedRegArray<6>, RegArray<8, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<7>, RegArray<8, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<8>, RegArray<8, 1>>);
-static_assert(std::is_same_v<DeducedRegArray<10>, RegArray<8, 2>>);
-static_assert(std::is_same_v<DeducedRegArray<18>, RegArray<8, 3>>);
 
-#elif defined __SSE__ || defined __ARM_NEON || defined __wasm32__
+// Should occupy a few 512 bit registers.
+static_assert(std::is_same_v<DeducedRegArray<9>, RegArray<8, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<16>, RegArray<8, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<17>, RegArray<8, 3>>);
+static_assert(std::is_same_v<DeducedRegArray<24>, RegArray<8, 3>>);
 
-// Should be a 128 bit register.
+#elif defined __SSE__
+
+// Can be fitted into a single 128 bit register.
+static_assert(std::is_same_v<DeducedRegArray<1>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<2>, RegArray<4, 1>>);
 static_assert(std::is_same_v<DeducedRegArray<3>, RegArray<4, 1>>);
+
+// Should occupy a few 128 bit registers.
 static_assert(std::is_same_v<DeducedRegArray<7>, RegArray<4, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<8>, RegArray<4, 2>>);
 static_assert(std::is_same_v<DeducedRegArray<9>, RegArray<4, 3>>);
+static_assert(std::is_same_v<DeducedRegArray<12>, RegArray<4, 3>>);
+
+#elif defined __ARM_NEON
+
+// Can be fitted into a single 64 bit register.
+static_assert(std::is_same_v<DeducedRegArray<1>, RegArray<2, 1>>);
+static_assert(std::is_same_v<DeducedRegArray<2>, RegArray<2, 1>>);
+
+// Can be fitted into a single 128 bit register.
+static_assert(std::is_same_v<DeducedRegArray<3>, RegArray<4, 1>>);
+static_assert(std::is_same_v<DeducedRegArray<4>, RegArray<4, 1>>);
+
+// Should occupy a few 128 bit registers.
+static_assert(std::is_same_v<DeducedRegArray<5>, RegArray<4, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<8>, RegArray<4, 2>>);
+static_assert(std::is_same_v<DeducedRegArray<9>, RegArray<4, 3>>);
+static_assert(std::is_same_v<DeducedRegArray<12>, RegArray<4, 3>>);
 
 #else
 
