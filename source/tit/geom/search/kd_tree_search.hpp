0032756727324e900e072d8053d1384010be7f6f
Oleg Butakov <butakovoleg@gmail.com>
2025-06-26 15:51:08 +0200

    Geom: Use recursive lambdas in KD tree.

--- source/tit/geom/search/kd_tree_search.hpp
+++ source/tit/geom/search/kd_tree_search.hpp
@@ -40,7 +40,7 @@ class KDTreeIndex final {
 public:
 
   /// Point type.
-  using Vec = std::ranges::range_value_t<Points>;
+  using Vec = point_range_vec_t<Points>;
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -50,8 +50,55 @@ public:
   explicit KDTreeIndex(Points points, size_t max_leaf_size = 1)
       : points_{std::move(points)}, max_leaf_size_{max_leaf_size} {
     TIT_ASSERT(max_leaf_size_ > 0, "Maximal leaf size should be positive.");
-    build_tree_();
+    if (std::ranges::empty(points_)) return;
+
+    // Initialize identity points permutation.
+    perm_ = iota_perm(points_) | std::ranges::to<std::vector>();
+
+    // Compute the root tree node (and bounding box).
+    par::TaskGroup tasks{};
+    const auto impl = [max_leaf_size_ = max_leaf_size,
+                       &points = points_,
+                       &pool = pool_,
+                       &tasks](this const auto& self, std::span<size_t> perm) {
+      // Compute bounding box.
+      const auto box = compute_bbox(points, perm);
+
+      // Is leaf node reached?
+      const auto node = pool.create();
+      if (perm.size() <= max_leaf_size_) {
+        // Fill the leaf node and end partitioning.
+        node->perm = perm;
+        node->left_subtree = node->right_subtree = nullptr;
+        return std::pair{node, box};
+      }
+
+      // Split the points based on the "widest" bounding box dimension.
+      const auto cut_axis = max_value_index(box.extents());
+      const auto center_coord = box.clamp(box.center())[cut_axis];
+      const auto [left_perm, right_perm] =
+          coord_bisection(points, perm, center_coord, cut_axis);
+
+      // Build subtrees.
+      node->cut_axis = cut_axis;
+      tasks.run(is_async_(left_perm), [left_perm, node, &self] {
+        const auto [left_tree, left_box] = self(left_perm);
+        node->left_subtree = left_tree;
+        node->cut_left = left_box.high()[node->cut_axis];
+      });
+      tasks.run(is_async_(right_perm), [right_perm, node, &self] {
+        const auto [right_tree, right_box] = self(right_perm);
+        node->right_subtree = right_tree;
+        node->cut_right = right_box.low()[node->cut_axis];
+      });
+
+      return std::pair{node, box};
+    };
+    std::tie(root_node_, tree_box_) = impl(perm_);
+    tasks.wait();
   }
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Find the points within the radius to the given point.
   template<std::output_iterator<size_t> OutIter,
@@ -61,81 +108,73 @@ public:
               OutIter out,
               Pred pred = {}) const -> OutIter {
     TIT_ASSERT(search_radius > 0.0, "Search radius should be positive.");
-    return search_tree_(search_point, search_radius, out, pred);
+
+    // Compute distance from the query point to the root bounding box
+    // per each dimension. (By "dist" square distances are meant.)
+    const auto search_dist = pow2(search_radius);
+    auto dists = pow2(search_point - tree_box_.clamp(search_point));
+
+    // Recursively search the tree.
+    const auto impl = [&points = points_, //
+                       &dists,
+                       &search_point,
+                       &search_dist,
+                       &out,
+                       &pred](this const auto& self,
+                              const KDTreeNode_* node) -> void {
+      TIT_ASSERT(node != nullptr, "Invalid tree node!");
+
+      const auto* const first_node = node->left_subtree;
+      const auto* const second_node = node->right_subtree;
+      if (first_node == nullptr) {
+        TIT_ASSERT(second_node == nullptr, "Invalid leaf node!");
+
+        // Collect points within the leaf node.
+        out = copy_points_near(points,
+                               node->perm,
+                               out,
+                               search_point,
+                               search_dist,
+                               pred);
+        return;
+      }
+
+      // Determine which branch should be taken first.
+      const auto cut_axis = node->cut_axis;
+      // const auto delta_left = search_point[cut_axis] - node->cut_left;
+      // const auto delta_right = node->cut_right - search_point[cut_axis];
+      // const auto [cut_dist, first_node, second_node] =
+      //     delta_left < delta_right ?
+      //         // Point is on the left to the cut plane, so the
+      //         // corresponding subtree should be searched first.
+      //         std::tuple{pow2(delta_right),
+      //                    node->left_subtree,
+      //                    node->right_subtree} :
+      //         // Point is on the right to the cut plane, so the
+      //         // corresponding subtree should be searched first.
+      //         std::tuple{pow2(delta_left),
+      //                    node->right_subtree,
+      //                    node->left_subtree};
+      vec_num_t<Vec> cut_dist{};
+
+      // Search in the first subtree.
+      self(first_node);
+
+      // Search in the second subtree (if it not too far).
+      if (sum(dists) < search_dist) {
+        const auto old_cut_dist = std::exchange(dists[cut_axis], cut_dist);
+        self(second_node);
+        dists[cut_axis] = old_cut_dist;
+      }
+    };
+    impl(root_node_);
+
+    return out;
   }
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 private:
-
-  // K-dimensional tree node structure.
-  union KDTreeNode_ {
-    std::span<const size_t> perm{};
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-    struct {
-      size_t cut_axis;
-      vec_num_t<Vec> cut_left;
-      vec_num_t<Vec> cut_right;
-      KDTreeNode_* left_subtree;
-      KDTreeNode_* right_subtree;
-    };
-#pragma GCC diagnostic pop
-  }; // union KDTreeNode_
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  // Build the K-dimensional tree.
-  void build_tree_() {
-    if (std::ranges::empty(points_)) return;
-
-    // Initialize identity points permutation.
-    perm_ = iota_perm(points_) | std::ranges::to<std::vector>();
-
-    // Compute the root tree node (and bounding box).
-    par::TaskGroup tasks{};
-    std::tie(root_node_, tree_box_) = build_subtree_(tasks, perm_);
-    tasks.wait();
-  }
-
-  // Build the K-dimensional subtree.
-  auto build_subtree_(par::TaskGroup& tasks, std::span<size_t> perm)
-      -> std::pair<KDTreeNode_*, BBox<Vec>> {
-    // Compute bounding box.
-    const auto box = compute_bbox(points_, perm);
-
-    // Is leaf node reached?
-    const auto node = pool_.create();
-    if (perm.size() <= max_leaf_size_) {
-      // Fill the leaf node and end partitioning.
-      node->perm = perm;
-      node->left_subtree = node->right_subtree = nullptr;
-      return {node, box};
-    }
-
-    // Split the points based on the "widest" bounding box dimension.
-    const auto cut_axis = max_value_index(box.extents());
-    const auto center_coord = box.clamp(box.center())[cut_axis];
-    const auto [left_perm, right_perm] =
-        coord_bisection(points_, perm, center_coord, cut_axis);
-
-    // Build subtrees.
-    /// @todo All these `lp = left_perm` shenanigans are a workaround for
-    ///       a Clang Analyzer 20.1.3 crash. Remove them once Clang is fixed.
-    node->cut_axis = cut_axis;
-    tasks.run(is_async_(left_perm), [lp = left_perm, node, &tasks, this] {
-      const auto [left_tree, left_box] = build_subtree_(tasks, lp);
-      node->left_subtree = left_tree;
-      node->cut_left = left_box.high()[node->cut_axis];
-    });
-    tasks.run(is_async_(right_perm), [rp = right_perm, node, &tasks, this] {
-      const auto [right_tree, right_box] = build_subtree_(tasks, rp);
-      node->right_subtree = right_tree;
-      node->cut_right = right_box.low()[node->cut_axis];
-    });
-
-    return {node, box};
-  }
 
   // Should the building be done in parallel?
   static auto is_async_(std::span<size_t> perm) noexcept -> bool {
@@ -143,96 +182,21 @@ private:
     return std::size(perm) >= parallel_threshold;
   }
 
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  // Search for the point neighbors in the K-dimensional tree.
-  //
-  /// @todo Search is slow for some reason. Investigate.
-  template<std::output_iterator<size_t> OutIter,
-           std::predicate<size_t> Pred = AlwaysTrue>
-  auto search_tree_(const Vec& search_point,
-                    vec_num_t<Vec> search_radius,
-                    OutIter out,
-                    Pred pred = {}) const -> OutIter {
-    // Compute distance from the query point to the root bounding box
-    // per each dimension. (By "dist" square distances are meant.)
-    const auto search_dist = pow2(search_radius);
-    auto dists = pow2(search_point - tree_box_.clamp(search_point));
-
-    // Recursively search the tree.
-    TIT_ASSERT(root_node_ != nullptr, "Tree was not built!");
-    return search_subtree_(root_node_,
-                           dists,
-                           search_point,
-                           search_dist,
-                           out,
-                           pred);
-  }
-
-  // Search for the point neighbors in the K-dimensional subtree.
-  // Parameters are passed by references in order to minimize stack usage.
-  template<std::output_iterator<size_t> OutIter,
-           std::predicate<size_t> Pred = AlwaysTrue>
-  auto search_subtree_(const KDTreeNode_* node,
-                       Vec& dists,
-                       const Vec& search_point,
-                       vec_num_t<Vec> search_dist,
-                       OutIter out,
-                       Pred pred) const -> OutIter {
-    if (node->left_subtree == nullptr) {
-      TIT_ASSERT(node->right_subtree == nullptr, "Invalid leaf node!");
-      // Collect points within the leaf node.
-      out = copy_points_near(points_,
-                             node->perm,
-                             out,
-                             search_point,
-                             search_dist,
-                             pred);
-      return out;
-    }
-
-    // Determine which branch should be taken first.
-    const auto cut_axis = node->cut_axis;
-    const auto [cut_dist, first_node, second_node] = [&] {
-      const auto delta_left = search_point[cut_axis] - node->cut_left;
-      const auto delta_right = node->cut_right - search_point[cut_axis];
-      return delta_left < delta_right ?
-                 // Point is on the left to the cut plane, so the
-                 // corresponding subtree should be searched first.
-                 std::tuple{pow2(delta_right),
-                            node->left_subtree,
-                            node->right_subtree} :
-                 // Point is on the right to the cut plane, so the
-                 // corresponding subtree should be searched first.
-                 std::tuple{pow2(delta_left),
-                            node->right_subtree,
-                            node->left_subtree};
-    }();
-
-    // Search in the first subtree.
-    out = search_subtree_(first_node,
-                          dists,
-                          search_point,
-                          search_dist,
-                          out,
-                          pred);
-
-    // Search in the second subtree (if it not too far).
-    if (const auto dist = sum(dists); dist < search_dist) {
-      const auto old_cut_dist = std::exchange(dists[cut_axis], cut_dist);
-      out = search_subtree_(second_node,
-                            dists,
-                            search_point,
-                            search_dist,
-                            out,
-                            pred);
-      dists[cut_axis] = old_cut_dist;
-    }
-
-    return out;
-  }
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  struct KDTreeNode_ {
+    KDTreeNode_* left_subtree = nullptr;
+    KDTreeNode_* right_subtree = nullptr;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+    union {
+      std::span<const size_t> perm{};
+      struct {
+        size_t cut_axis;
+        vec_num_t<Vec> cut_left;
+        vec_num_t<Vec> cut_right;
+      };
+    };
+#pragma GCC diagnostic pop
+  };
 
   Points points_;
   size_t max_leaf_size_;
