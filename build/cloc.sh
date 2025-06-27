0bd4617d9868f253f9cbc73b84f29f26f9994ac9
Oleg Butakov <butakovoleg@gmail.com>
2025-06-27 22:43:55 +0200

    Build: Support various file types in formatting script.

--- build/cloc.sh
+++ build/cloc.sh
@@ -8,10 +8,14 @@
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Print usage information if requested.
 if [[ "$*" == *"-h"* || "$*" == *"--help"* ]]; then
   echo "Usage: $(basename "$0") [path ...]"
+  echo "Counts the lines of code in the git index."
   exit 1
 fi
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Count the lines of code.
 git ls-files "$@" | xargs cloc
