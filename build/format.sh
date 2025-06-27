850a6d151086e0e6c67a41da48027e4ec2a4c8c9
Oleg Butakov <butakovoleg@gmail.com>
2025-06-27 22:43:55 +0200

    Build: Support various file types in formatting script.

--- build/format.sh
+++ build/format.sh
@@ -8,7 +8,33 @@
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Print usage information if requested.
+if [[ "$*" == *"-h"* || "$*" == *"--help"* ]]; then
+  echo "Usage: $(basename "$0") [path ...]"
+  echo "Formats all indexed files in the git index."
+  exit 1
+fi
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # Run clang-format on all the C/C++ files.
-git ls-files | grep -E "\.(c|h)(pp)?$" | xargs clang-format -i
+echo "Formatting C/C++ files..."
+git ls-files "$@" |
+  grep -E "(.*/_cxx/.*)|(\.(c|h)(pp)?$)" |
+  xargs clang-format -i
+
+# Run Prettier on all the web files.
+echo "Formatting web files..."
+git ls-files "$@" |
+  grep -E "\.(js|ts|jsx|tsx|css|html|json)$" |
+  xargs prettier --write
+
+# Run YAPF on all the Python files.
+echo "Formatting Python files..."
+git ls-files "$@" |
+  grep -E "\.py$" |
+  xargs yapf -i
+
+echo "All done!"
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
