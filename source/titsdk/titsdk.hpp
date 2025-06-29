b863e70ba0a618073fafb94fef4d83e0d87643c7
Oleg Butakov <butakovoleg@gmail.com>
2025-06-30 00:05:10 +0200

    SDK: Setup a C/Python library.

---
+++ source/titsdk/titsdk.hpp
@@ -0,0 +1,9 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *\
+ * Part of BlueTit Solver, licensed under Apache 2.0 with Commons Clause.
+ * Commercial use, including SaaS, requires a separate license, see /LICENSE.md
+\* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// IWYU pragma: begin_exports
+#include "lib.hpp"
+#include "ttdb.hpp"
+// IWYU pragma: end_exports
--- source/ttdbreader/TTDBReader.py
+++ source/ttdbreader/TTDBReader.py
@@ -25,9 +25,9 @@ from vtk import (
 )
 from vtkmodules.util import numpy_support
 
-# ParaView does not know how to import `ttdb.py`.
-sys.path.append(os.path.dirname(__file__))
-from ttdb import (  # pylint: disable=wrong-import-position # noqa: E402
+# ParaView does not know how to import titsdk.
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "sdk"))
+from titsdk import (  # pylint: disable=wrong-import-position # noqa: E402
     Rank, Storage, TimeStep, open_storage,
 )
 
