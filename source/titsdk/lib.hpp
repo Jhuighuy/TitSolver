e0e13b475f6e337e958ea89f9ae06f82eed4a2d9
Oleg Butakov <butakovoleg@gmail.com>
2025-07-16 15:13:06 +0200

    SDK: Add Doxygen comments.

--- source/titsdk/lib.hpp
+++ source/titsdk/lib.hpp
@@ -13,8 +13,25 @@ extern "C" {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+/**
+ * Get the last error message.
+ *
+ * @returns Pointer to a null-terminated string containing the last error
+ *          message. If no error has occurred, returns null.
+ */
 const char* titsdk__last_error();
+
+/**
+ * Clear the last error message.
+ */
 void titsdk__clear_error();
+
+/**
+ * Set the last error message.
+ *
+ * @param error Pointer to a null-terminated string containing the error
+ *              message. This string will be copied internally.
+ */
 void titsdk__set_error(const char* error);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
