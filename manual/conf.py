e706d7b6dd7563ae4f10af7de408632726c79769
Oleg Butakov <butakovoleg@gmail.com>
2025-08-01 22:43:10 +0200

    Manual: Fix a few typos and CSS bugs.

--- manual/conf.py
+++ manual/conf.py
@@ -10,16 +10,27 @@
 
 # pylint: disable=invalid-name,redefined-builtin,missing-module-docstring
 import datetime
+import json
+import os
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Python configuration.
+#
+
+MANUAL_ROOT_DIR = os.path.dirname(__file__)
+PROJECT_ROOT_DIR = os.path.abspath(os.path.join(MANUAL_ROOT_DIR, ".."))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Project information.
 #
 
-project = "Tit Solver"
+project = "BlueTit Solver"
 copyright = f"{datetime.date.today().year}"
 author = "Oleg Butakov"
-release = "0.1"
+with open(os.path.join(PROJECT_ROOT_DIR, "vcpkg.json"), encoding="utf-8") as f:
+  release = json.load(f)["version-string"]
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
@@ -34,13 +45,16 @@ numfig_format = {
     "table": "Table %s.",
     "code-block": "Code %s.",
 }
+rst_prolog = r"""
+.. |product| replace:: :math:`\mathsf{BlueTit\text{ }Solver}`
+"""
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Options for HTML output.
 #
 
-html_short_title = "Tit Solver"
+html_short_title = "BlueTit Solver"
 html_copy_source = False
 html_permalinks = False
 html_theme = "basic"
