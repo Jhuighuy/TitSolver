11ab42e153c2a0444556a76afa9bd200b1931234
Oleg Butakov <butakovoleg@gmail.com>
2025-09-30 16:57:53 +0200

    simplify mat part spec

--- source/tit/core/_mat/part.hpp
+++ source/tit/core/_mat/part.hpp
@@ -10,7 +10,6 @@
 #include "tit/core/_mat/traits.hpp"
 #include "tit/core/basic_types.hpp"
 #include "tit/core/checks.hpp"
-#include "tit/core/enum.hpp"
 
 namespace tit {
 
@@ -29,8 +28,15 @@ enum class MatPart : uint8_t {
   transposed = 1 << 7,       ///< Transpose the matrix.
 };
 
-template<>
-inline constexpr bool is_flags_enum_v<MatPart> = true;
+/// Check if two matrix part flags enums intersect.
+constexpr auto operator&(MatPart f, MatPart g) noexcept -> bool {
+  return (std::to_underlying(f) & std::to_underlying(g)) != 0;
+}
+
+/// Merge two matrix part flags enums.
+constexpr auto operator|(MatPart f, MatPart g) noexcept -> MatPart {
+  return static_cast<MatPart>(std::to_underlying(f) | std::to_underlying(g));
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
