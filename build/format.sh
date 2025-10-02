28780e8c0887efedb791b667d9fca9237a0b1e17
Oleg Butakov <butakovoleg@gmail.com>
2025-10-02 22:21:48 +0200

    Build: Drop support for libc++.

--- build/format.sh
+++ build/format.sh
@@ -10,9 +10,9 @@
 
 # Print usage information if requested.
 if [[ "$*" == *"-h"* || "$*" == *"--help"* ]]; then
-  echo "Usage: $(basename "$0") [path ...]"
-  echo "Formats all indexed files in the git index."
-  exit 1
+	echo "Usage: $(basename "$0") [path ...]"
+	echo "Formats all indexed files in the git index."
+	exit 1
 fi
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,20 +20,20 @@ fi
 # Run clang-format on all the C/C++ files.
 echo "Formatting C/C++ files..."
 git ls-files "$@" |
-  grep -E "(.*/_cxx/.*)|(\.(c|h)(pp)?$)" |
-  xargs clang-format -i
+	grep -E "\.(c|h)(pp)?$" |
+	xargs clang-format -i
 
 # Run Prettier on all the web files.
 echo "Formatting web files..."
 git ls-files "$@" |
-  grep -E "\.(js|ts|jsx|tsx|css|html|json)$" |
-  xargs prettier --write
+	grep -E "\.(js|ts|jsx|tsx|css|html|json)$" |
+	xargs prettier --write
 
 # Run YAPF on all the Python files.
 echo "Formatting Python files..."
 git ls-files "$@" |
-  grep -E "\.py$" |
-  xargs yapf -i
+	grep -E "\.py$" |
+	xargs yapf -i
 
 echo "All done!"
 
