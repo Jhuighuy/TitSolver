ea46513eb979472451fcf1d050b7a016fca29a2e
Oleg Butakov <butakovoleg@gmail.com>
2025-10-16 13:07:52 +0200

    App: A new PySide6 backend. (#141)

---
+++ source/titback/__init__.py
@@ -0,0 +1,6 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Part of BlueTit Solver, under the MIT License.
+# See /LICENSE.md for license information. SPDX-License-Identifier: MIT
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This file is intentionally left blank.
