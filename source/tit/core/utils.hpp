e93e606b15bb9070dba3c74eaf8e83f704153ad8
Oleg Butakov <butakovoleg@gmail.com>
2025-11-14 20:57:44 +0100

    Assorted clean-ups.

--- source/tit/core/utils.hpp
+++ source/tit/core/utils.hpp
@@ -28,9 +28,6 @@ namespace tit {
 /// to avoid false alarms from analysis tools.
 #define TIT_ASSUME_UNIVERSAL(T, ref) static_cast<void>(std::forward<T>(ref))
 
-/// Forward a member of a class.
-#define TIT_FORWARD_LIKE(self, member) std::forward_like<decltype(self)>(member)
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// Default-initialize a type.
